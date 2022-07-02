55ef9df11b8723817fa7a61701d33197a414d251
meatsby <meatsby@gmail.com>
2022-07-02 15:46:28 +0900

    feat: 환경설정을 세팅한다

---
+++ backend/gong-check/src/test/java/com/woowacourse/gongcheck/GongCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.gongcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
