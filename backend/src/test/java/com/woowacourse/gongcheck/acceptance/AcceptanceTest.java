2425474d3e90867cf81a9a97b991fae146f2981e
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-07-03 20:39:46 +0900

    test: 인수테스트 세팅 클래스 추가한다

---
+++ backend/src/test/java/com/woowacourse/gongcheck/acceptance/AcceptanceTest.java
@@ -0,0 +1,21 @@
+package com.woowacourse.gongcheck.acceptance;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class AcceptanceTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setPort() {
+        RestAssured.port = port;
+    }
+}
