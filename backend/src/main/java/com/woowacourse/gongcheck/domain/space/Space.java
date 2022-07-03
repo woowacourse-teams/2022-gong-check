5731c8d07e54e6fbc3c6fe8112c2bed1512af3d5
beomWhale <cndqjacndqja@gmail.com>
2022-07-03 16:57:08 +0900

    feat: Space Entity를 작성한다

---
+++ backend/src/main/java/com/woowacourse/gongcheck/domain/space/Space.java
@@ -0,0 +1,75 @@
+package com.woowacourse.gongcheck.domain.space;
+
+import com.woowacourse.gongcheck.domain.member.Member;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Table(name = "space")
+@Getter
+public class Space {
+
+    private static final int NAME_MAX_LENGTH = 20;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "name", length = NAME_MAX_LENGTH, nullable = false)
+    private String name;
+
+    @Column(name = "img_url")
+    private String imageUrl;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    protected Space() {
+    }
+
+    @Builder
+    public Space(Long id, Member member, String name, String imageUrl, LocalDateTime createdAt,
+      LocalDateTime updatedAt) {
+        this.id = id;
+        this.member = member;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Space space = (Space) o;
+        return id.equals(space.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
