932e79098852de63082f97ff77dbe567d9b925b0
최진영 <jinyoungchoi95@gmail.com>
2022-07-03 21:04:29 +0900

    [BE] 인수테스트, 문서화테스트 환경을 세팅한다. (#11)
    * test: 인수테스트 세팅 클래스 추가한다
    
    * chore: rest docs 세팅 스크립트를 추가한다
    
    * test: Documentation 문서화 세팅 테스트를 추가한다

---
+++ backend/src/test/java/com/woowacourse/gongcheck/documentation/DocumentationTest.java
@@ -0,0 +1,34 @@
+package com.woowacourse.gongcheck.documentation;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@WebMvcTest({
+
+})
+@ExtendWith(RestDocumentationExtension.class)
+class DocumentationTest {
+
+    protected MockMvcRequestSpecification docsGiven;
+
+    @BeforeEach
+    void setDocsGiven(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        docsGiven = RestAssuredMockMvc.given()
+                .mockMvc(MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                        .apply(documentationConfiguration(restDocumentation)
+                                .operationPreprocessors()
+                                .withRequestDefaults(prettyPrint())
+                                .withResponseDefaults(prettyPrint()))
+                        .build()).log().all();
+    }
+}
