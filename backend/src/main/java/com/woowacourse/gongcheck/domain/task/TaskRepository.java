a9bf36296d27551dd75c2ff50daf89b5764626d1
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-07-06 03:03:04 +0900

    feat: Job이 가진 모든 Task 반환기능을 구현한다

--- backend/src/main/java/com/woowacourse/gongcheck/domain/task/TaskRepository.java
+++ backend/src/main/java/com/woowacourse/gongcheck/domain/task/TaskRepository.java
@@ -1,6 +1,10 @@
 package com.woowacourse.gongcheck.domain.task;
 
+import com.woowacourse.gongcheck.domain.job.Job;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAllBySectionJob(final Job job);
 }
