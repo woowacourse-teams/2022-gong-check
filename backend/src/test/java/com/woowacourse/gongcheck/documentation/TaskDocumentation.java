129427c58c0ba6145616e2c74fb19e0e440a927a
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-07-06 03:35:01 +0900

    feat: 새로운 진행작업 생성 api 구현 및 documentation 추가한다

---
+++ backend/src/test/java/com/woowacourse/gongcheck/documentation/TaskDocumentation.java
@@ -0,0 +1,26 @@
+package com.woowacourse.gongcheck.documentation;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+class TaskDocumentation extends DocumentationTest {
+
+    @Test
+    void 새_진행_작업_생성() {
+        doNothing().when(taskService).createNewRunningTask(anyLong(), any());
+        when(authenticationContext.getPrincipal()).thenReturn(String.valueOf(anyLong()));
+
+        docsGiven
+                .header("Authorization", "Bearer jwt.token.here")
+                .when().post("/api/jobs/1/tasks/new")
+                .then().log().all()
+                .apply(document("tasks/create"))
+                .statusCode(HttpStatus.CREATED.value());
+    }
+}
