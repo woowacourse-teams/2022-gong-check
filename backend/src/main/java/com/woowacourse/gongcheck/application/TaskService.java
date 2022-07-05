eb7509442fdb2e60af19d118b055daae3339be70
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-07-06 03:15:11 +0900

    feat: 이미 진행중인 작업이 존재하는데 생성 시 예외기능을 구현한다

--- backend/src/main/java/com/woowacourse/gongcheck/application/TaskService.java
+++ backend/src/main/java/com/woowacourse/gongcheck/application/TaskService.java
@@ -5,6 +5,9 @@ import com.woowacourse.gongcheck.domain.job.JobRepository;
 import com.woowacourse.gongcheck.domain.member.Member;
 import com.woowacourse.gongcheck.domain.member.MemberRepository;
 import com.woowacourse.gongcheck.domain.task.RunningTaskRepository;
+import com.woowacourse.gongcheck.domain.task.TaskRepository;
+import com.woowacourse.gongcheck.domain.task.Tasks;
+import com.woowacourse.gongcheck.exception.BusinessException;
 import com.woowacourse.gongcheck.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,12 +18,14 @@ public class TaskService {
 
     private final MemberRepository memberRepository;
     private final JobRepository jobRepository;
+    private final TaskRepository taskRepository;
     private final RunningTaskRepository runningTaskRepository;
 
     public TaskService(final MemberRepository memberRepository, final JobRepository jobRepository,
-                       final RunningTaskRepository runningTaskRepository) {
+                       final TaskRepository taskRepository, final RunningTaskRepository runningTaskRepository) {
         this.memberRepository = memberRepository;
         this.jobRepository = jobRepository;
+        this.taskRepository = taskRepository;
         this.runningTaskRepository = runningTaskRepository;
     }
 
@@ -30,5 +35,10 @@ public class TaskService {
                 .orElseThrow(() -> new NotFoundException("존재하지 않는 호스트입니다."));
         Job job = jobRepository.findBySpaceMemberAndId(host, jobId)
                 .orElseThrow(() -> new NotFoundException("존재하지 않는 작업입니다."));
+
+        Tasks tasks = new Tasks(taskRepository.findAllBySectionJob(job));
+        if (runningTaskRepository.existsByTaskIdIn(tasks.getTaskIds())) {
+            throw new BusinessException("현재 진행중인 작업이 존재하여 새로운 작업을 생성할 수 없습니다.");
+        }
     }
 }
---
+++ backend/src/main/java/com/woowacourse/gongcheck/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.woowacourse.gongcheck.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(final String message) {
+        super(message);
+    }
+}
