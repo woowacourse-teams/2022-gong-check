762fa9b4c8299d964e9cd08a873e0571df2d1a8b
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-07-06 10:27:56 +0900

    test: Job 보유 task 조회 시 데이터를 추가한다

--- backend/src/test/java/com/woowacourse/gongcheck/domain/task/TaskRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/gongcheck/domain/task/TaskRepositoryTest.java
@@ -43,11 +43,13 @@ class TaskRepositoryTest {
         Member host = memberRepository.save(Member_생성("1234"));
         Space space = spaceRepository.save(Space_생성(host, "잠실"));
         Job job = jobRepository.save(Job_생성(space, "청소"));
-        Section section = sectionRepository.save(Section_생성(job, "트랙룸"));
-        taskRepository.saveAll(List.of(Task_생성(section, "책상 청소"), Task_생성(section, "의자 넣기")));
+        Section section1 = sectionRepository.save(Section_생성(job, "트랙룸"));
+        Section section2 = sectionRepository.save(Section_생성(job, "굿샷 강의장"));
+        taskRepository.saveAll(List.of(Task_생성(section1, "책상 청소"), Task_생성(section1, "빈백 정리")));
+        taskRepository.saveAll(List.of(Task_생성(section2, "책상 청소"), Task_생성(section2, "의자 넣기")));
 
         List<Task> result = taskRepository.findAllBySectionJob(job);
 
-        assertThat(result).hasSize(2);
+        assertThat(result).hasSize(4);
     }
 }
