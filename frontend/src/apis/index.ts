4206861cfc10e96aca8b1e66957650fa7d12d285
intae92 <56149367+intae92@users.noreply.github.com>
2022-07-07 12:38:05 +0900

    [FE] API 호출 방식을 설계한다. (#71)
    * chore: emotion, react-icons 설치
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: Header UI컴포넌트 구현
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * chore: 라이브러리 의존성 변경
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: PageTitle 컴포넌트 분리
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: JobList 페이지에 pageTitle 컴포넌트 연동
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: CardTitle 컴포넌트 분리
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: SpaceCard 컴포넌트 분리
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: JobCard 컴포넌트 분리
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * design: Header 컴포넌트 그림자 노출 수정
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 체크리스트 페이지 UI 구현, checkbox 컴포넌트 구현
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 공용 Button 컴포넌트 UI구현
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 페이지 이동 기능 구현
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: GlobalStyle, Theme 적용
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * style: 헤더 뒤로가기 화살표 버튼 색 변경
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 모달 컴포넌트 구현
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * fix: 모달 창 닫기 기능, 이벤트 버블링 막기
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 헤더에 이미지 로고 보여주기
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: import 절대경로 및 자동정렬 세팅
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: api 구조 설계
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

---
+++ frontend/src/apis/index.ts
@@ -0,0 +1,109 @@
+import axios from 'axios';
+
+const DEV_URL = 'http://localhost:8080';
+// const PROD_URL = process.env.REACT_APP_API_ROOT;
+// const BASE_URL = '';
+const API_URL = DEV_URL;
+
+const TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjU3MTYwMDU2LCJleHAiOjE2NTcxNjM2NTZ9.o63RFWZc3-duoCoVID3fX943pb1b4NwpxXiQ8QNnzyI';
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+});
+
+const axiosInstanceToken = axios.create({
+  baseURL: API_URL,
+});
+
+axiosInstanceToken.interceptors.request.use(
+  config => {
+    // const accessToken = sessionStorage.getItem('accessToken');
+    const accessToken = TOKEN;
+
+    if (accessToken) {
+      config.headers = {
+        Authorization: `Bearer ${accessToken}`,
+      };
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// 비밀번호 입력
+const postPassword = async ({ hostId, password }: any) => {
+  return await axiosInstance({
+    method: 'POST',
+    url: `api/hosts/${hostId}/enter`,
+    data: {
+      password,
+    },
+  });
+};
+
+// 공간 목록 {page,size}
+const getSpaces = async () => {
+  return await axiosInstanceToken({
+    method: 'GET',
+    url: `api/spaces`,
+  });
+};
+
+// 업무 종류
+const getJobs = async ({ spaceId }: any) => {
+  return await axiosInstanceToken({
+    method: 'GET',
+    url: `/api/spaces/${spaceId}/jobs`,
+  });
+};
+
+// 진행중인 작업이 있는지 확인
+const getJobActive = async ({ jobId }: any) => {
+  return await axiosInstanceToken({
+    method: 'GET',
+    url: `/api/jobs/${jobId}/active`,
+  });
+};
+
+// 새 작업 생성
+const postNewTasks = async ({ jobId }: any) => {
+  return await axiosInstanceToken({
+    method: 'POST',
+    url: `/api/jobs/${jobId}/tasks/new`,
+  });
+};
+
+// 진행중인 작업 불러오기
+const getTasks = async ({ jobId }: any) => {
+  return await axiosInstanceToken({
+    method: 'GET',
+    url: `/api/jobs/${jobId}/tasks`,
+  });
+};
+
+// 체크박스 체크하기
+const postCheckTask = async ({ taskId }: any) => {
+  return await axiosInstanceToken({
+    method: 'POST',
+    url: `/api/tasks/${taskId}/flip`,
+  });
+};
+
+// 제출하기
+const postJobComplete = async ({ jobId, author }: any) => {
+  return await axiosInstanceToken({
+    method: 'POST',
+    url: `/api/jobs/${jobId}/complete`,
+    data: {
+      author,
+    },
+  });
+};
+
+const apis = { postPassword, getSpaces, getJobs, getJobActive, postNewTasks, getTasks, postCheckTask, postJobComplete };
+
+export default apis;
