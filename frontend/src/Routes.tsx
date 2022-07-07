120bc8599213bb33986844fe1ffc4cea814486ae
intae92 <56149367+intae92@users.noreply.github.com>
2022-07-07 12:43:07 +0900

    [FE] 공간 입장 API를 연동한다. (#74)
    * chore: emotion, react-icons 설치
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: Header UI컴포넌트 구현
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * chore: 라이브러리 의존성 변경
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: PageTitle 컴포넌트 분리
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: JobList 페이지에 pageTitle 컴포넌트 연동
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: CardTitle 컴포넌트 분리
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: SpaceCard 컴포넌트 분리
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: JobCard 컴포넌트 분리
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * design: Header 컴포넌트 그림자 노출 수정
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 체크리스트 페이지 UI 구현, checkbox 컴포넌트 구현
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 공용 Button 컴포넌트 UI구현
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 페이지 이동 기능 구현
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: GlobalStyle, Theme 적용
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * style: 헤더 뒤로가기 화살표 버튼 색 변경
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 모달 컴포넌트 구현
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * fix: 모달 창 닫기 기능, 이벤트 버블링 막기
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 헤더에 이미지 로고 보여주기
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: import 절대경로 및 자동정렬 세팅
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: api 구조 설계
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    * feat: 공간 입장 API 연동
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>
    
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,13 +1,29 @@
-import Header from './components/Header';
-import JobList from './pages/JobList';
-import SpaceList from './pages/SpaceList';
-import TaskList from './pages/TaskList';
 import { Outlet } from 'react-router-dom';
 
+import JobList from '@/pages/JobList';
+import SpaceList from '@/pages/SpaceList';
+import TaskList from '@/pages/TaskList';
+
+import Header from '@/components/Header';
+import InputModal from '@/components/InputModal';
+
+import useModal from '@/hooks/useModal';
+
 const DefaultLayout = () => {
+  const { isShowModal, closeModal } = useModal(!localStorage.getItem('user'));
+
   return (
     <>
       <Header />
+      {isShowModal && (
+        <InputModal
+          title="비밀번호 입력"
+          detail="해당 공간의 관계자만 접근할 수 있습니다."
+          placeholder="비밀번호를 입력해주세요."
+          buttonText="확인"
+          closeModal={closeModal}
+        />
+      )}
       <Outlet />
     </>
   );
