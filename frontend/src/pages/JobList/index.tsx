4b9f1759956ea4badd8a599e044636df43c9fbe2
intae92 <kangit2013@gmail.com>
2022-07-07 13:52:32 +0900

    feat: 업무 목록 불러오기 API 연동
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

--- frontend/src/pages/JobList/index.tsx
+++ frontend/src/pages/JobList/index.tsx
@@ -1,22 +1,36 @@
 /**  @jsxImportSource @emotion/react */
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 import PageTitle from '@/components/_common/PageTitle';
 
 import JobCard from '@/components/JobCard';
 
+import apis from '@/apis';
+
 import styles from './styles';
 
-const jobs = [
-  {
-    id: 1,
-    name: '청소',
-  },
-  {
-    id: 2,
-    name: '마감',
-  },
-];
+type JobType = {
+  id: number;
+  name: string;
+};
 
 const JobList = () => {
+  const [jobs, setJobs] = useState<Array<JobType>>([]);
+  const { spaceId } = useParams();
+
+  useEffect(() => {
+    const getSpaces = async () => {
+      const {
+        data: { jobs },
+      } = await apis.getJobs({ spaceId });
+
+      setJobs(jobs);
+    };
+
+    getSpaces();
+  }, []);
+
   return (
     <div css={styles.layout}>
       <PageTitle>
