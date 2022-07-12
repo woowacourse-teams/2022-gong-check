2e001b49c4e247bb3b236a99b7d4cbe41f5e04da
awesomeo184 <awesomeo184@gmail.com>
2022-07-12 16:49:30 +0900

    refactor: final 키워드 추가

--- backend/src/main/java/com/woowacourse/gongcheck/domain/host/HostRepository.java
+++ backend/src/main/java/com/woowacourse/gongcheck/domain/host/HostRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HostRepository extends JpaRepository<Host, Long> {
 
-    default Host getById(Long hostId) throws NotFoundException {
-        return findById(hostId).orElseThrow(() -> new NotFoundException("존재하지 않는 호스트입니다."));
+    default Host getById(final Long id) throws NotFoundException {
+        return findById(id).orElseThrow(() -> new NotFoundException("존재하지 않는 호스트입니다."));
     }
 }
