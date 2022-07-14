ae83a0f072ea7f240f21c43bd46d59b60697e1a9
cks3066 <cks3066@naver.com>
2022-07-14 17:34:06 +0900

    feat: 깃허브 로그인 버튼

---
+++ frontend/src/components/_common/GitHubLoginButton/styles.ts
@@ -0,0 +1,20 @@
+import { css } from '@emotion/react';
+
+const wrapper = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 240px;
+  border-radius: 24px;
+  color: white;
+  background-color: #21262c;
+`;
+
+const text = css`
+  margin-left: 16px;
+  font-size: 24px;
+`;
+
+const styles = { wrapper, text };
+
+export default styles;
