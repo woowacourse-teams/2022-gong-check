e8e3e3d509441ad46f25b47bbcc730eeebc41c7f
Seungchan On <62434898+cks3066@users.noreply.github.com>
2022-07-15 21:36:32 +0900

    [FE] 사용자가 공간을 선택할 수 있는 페이지 UI 수정 (#143)
    * style: html lang "en" 에서 "ko"로 변경
    
    * feat: SpaceCard 컴포넌트 수정
    
    * refactor: SpaceList 페이지 수정
    
    * refactor: 글로벌 스타일 수정
    
    * refactor: UserLayour 헤더 제거
    
    * feat: 로고의 이미지를 변경한다.

--- frontend/src/pages/user/SpaceList/index.tsx
+++ frontend/src/pages/user/SpaceList/index.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from 'react-query';
 
-import PageTitle from '@/components/_common/PageTitle';
-
 import SpaceCard from '@/components/SpaceCard';
 
 import apis from '@/apis';
+
+import logo from '@/assets/logoTitle.png';
 
 import styles from './styles';
 
@@ -13,14 +13,11 @@ const SpaceList: React.FC = () => {
 
   return (
     <div css={styles.layout}>
-      <PageTitle>
-        장소 목록 (<span>{data?.spaces.length}</span>)
-      </PageTitle>
-      <div css={styles.contents}>
-        {data?.spaces.map(space => (
-          <SpaceCard spaceName={space.name} imageUrl={space.imageUrl} key={space.id} id={space.id} />
-        ))}
-      </div>
+      <img css={styles.logo} src={logo} alt="공간 체크" />
+      <span css={styles.text}>사용하실 공간을 선택해주세요.</span>
+      {data?.spaces.map(space => (
+        <SpaceCard spaceName={space.name} imageUrl={space.imageUrl} key={space.id} id={space.id} />
+      ))}
     </div>
   );
 };
