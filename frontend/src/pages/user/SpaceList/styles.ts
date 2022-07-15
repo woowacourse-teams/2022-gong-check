1b1a9c2999cc30d319298eb34d84659a997b5488
cks3066 <cks3066@naver.com>
2022-07-15 17:38:53 +0900

    refactor: SpaceList 페이지 수정

--- frontend/src/pages/user/SpaceList/styles.ts
+++ frontend/src/pages/user/SpaceList/styles.ts
@@ -1,16 +1,24 @@
 import { css } from '@emotion/react';
 
 const layout = css`
-  padding: 8px 32px;
-`;
-
-const contents = css`
   display: flex;
   flex-direction: column;
   width: 100%;
   align-items: center;
+  padding: 32px;
 `;
 
-const styles = { layout, contents };
+const logo = css`
+  width: 320px;
+  margin-bottom: 48px;
+`;
+
+const text = css`
+  font-weight: 600;
+  color: #84817a;
+  margin-bottom: 24px;
+`;
+
+const styles = { layout, logo, text };
 
 export default styles;
