bb841ddf2f9f6b405c75910c69b242b8e33b5df2
intae92 <kangit2013@gmail.com>
2022-07-15 20:23:23 +0900

    feat: 공간 정보 submit 버튼 활성화 기능 적용

--- frontend/src/components/host/SpaceInfo/index.tsx
+++ frontend/src/components/host/SpaceInfo/index.tsx
@@ -1,22 +1,26 @@
-import Button from '../../_common/Button';
 import { css } from '@emotion/react';
 import React, { useMemo, useRef, useState } from 'react';
 import { HiPlus } from 'react-icons/hi';
+
+import Button from '@/components/_common/Button';
+
+import theme from '@/styles/theme';
 
 import styles from './styles';
 
 type SpaceInfoProps = {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-  inputText: string;
+  inputText: '' | string;
 };
 
-const SpaceInfo: React.FC<SpaceInfoProps> = ({ onSubmit, inputText }) => {
+const SpaceInfo: React.FC<SpaceInfoProps> = ({ onSubmit, inputText = '' }) => {
   const labelRef = useRef(null);
   const [imgURL, setImgURL] = useState('');
+  const [isActiveSubmit, setIsActiveSubmit] = useState(false);
 
   const isExistImgURL = useMemo(() => !!imgURL, [imgURL]);
 
-  const onChangeImg = (e: React.FormEvent<HTMLInputElement>) => {
+  const handleChangeImg = (e: React.FormEvent<HTMLInputElement>) => {
     const input = e.target as HTMLInputElement;
 
     if (!input.files?.length || !labelRef.current) {
@@ -29,13 +33,26 @@ const SpaceInfo: React.FC<SpaceInfoProps> = ({ onSubmit, inputText }) => {
     setImgURL(src);
   };
 
+  const handleChangeSpaceName = (e: React.FormEvent<HTMLInputElement>) => {
+    const input = e.target as HTMLInputElement;
+
+    const isExistValue = input.value.length > 0;
+    setIsActiveSubmit(isExistValue);
+  };
+
   return (
     <div css={styles.spaceInfo}>
       <form onSubmit={onSubmit}>
         <div css={styles.titleWrapper}>
           <p css={styles.title}>공간 정보</p>
-          <Button type="submit" css={styles.button}>
-            수정 하기
+          <Button
+            type="submit"
+            css={css`
+              ${styles.button}
+              background: ${isActiveSubmit ? theme.colors.primary : theme.colors.gray};
+            `}
+          >
+            생성하기
           </Button>
         </div>
         <div css={styles.imageContainer}>
@@ -50,7 +67,7 @@ const SpaceInfo: React.FC<SpaceInfoProps> = ({ onSubmit, inputText }) => {
               htmlFor="file"
               ref={labelRef}
             >
-              <input css={styles.imageInput} type="file" id="file" required accept="image/*" onChange={onChangeImg} />
+              <input css={styles.imageInput} type="file" id="file" accept="image/*" onChange={handleChangeImg} />
               {!isExistImgURL && (
                 <div css={styles.iconBox}>
                   <HiPlus size={50} />
@@ -65,7 +82,14 @@ const SpaceInfo: React.FC<SpaceInfoProps> = ({ onSubmit, inputText }) => {
         <div css={styles.inputContainer}>
           <div css={styles.inputWrapper}>
             <p css={styles.subTitle}>공간 이름</p>
-            <input css={styles.input} placeholder="이름을 입력하세요" type="text" />
+            <input
+              css={styles.input}
+              placeholder="이름을 입력하세요"
+              type="text"
+              defaultValue={inputText || ''}
+              onChange={handleChangeSpaceName}
+              required
+            />
           </div>
         </div>
       </form>
--- frontend/src/pages/host/SpaceCreate/index.tsx
+++ frontend/src/pages/host/SpaceCreate/index.tsx
@@ -3,7 +3,7 @@ import SpaceInfo from '@/components/host/SpaceInfo';
 import styles from './styles';
 
 const SpaceCreate: React.FC = () => {
-  const spaceName = '잠실 캠퍼스';
+  const spaceName = '';
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log('click');
