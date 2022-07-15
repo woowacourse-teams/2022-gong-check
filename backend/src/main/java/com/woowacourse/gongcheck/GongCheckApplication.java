bb9f2f218d71bb1a6eae30727bc599d15fe02edc
meatsby <meatsby@gmail.com>
2022-07-15 16:50:15 +0900

    refactor: 슬랙 알림 비동기 처리 기능 추가

--- backend/src/main/java/com/woowacourse/gongcheck/GongCheckApplication.java
+++ backend/src/main/java/com/woowacourse/gongcheck/GongCheckApplication.java
@@ -2,12 +2,13 @@ package com.woowacourse.gongcheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class GongCheckApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GongCheckApplication.class, args);
     }
-
 }
--- backend/src/main/java/com/woowacourse/gongcheck/application/SlackAPIService.java
+++ backend/src/main/java/com/woowacourse/gongcheck/application/SlackAPIService.java
@@ -4,22 +4,20 @@ import com.slack.api.Slack;
 import com.slack.api.webhook.Payload;
 import com.woowacourse.gongcheck.application.response.Attachments;
 import com.woowacourse.gongcheck.application.response.SubmissionResponse;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 public class SlackAPIService implements SlackService {
 
     @Override
+    @Async
     public void sendMessage(final SubmissionResponse submissionResponse) {
-        try {
-            Slack slack = Slack.getInstance();
+        try (Slack slack = Slack.getInstance()) {
             Payload payload = Payload.builder()
                     .attachments(Attachments.of(submissionResponse).getAttachments())
                     .build();
             slack.send(submissionResponse.getSlackUrl(), payload);
-            slack.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
