0e721923e913e6d7bc9b7ca461e8a388feb75fc3
김찬 <gmelddl514@gmail.com>
2022-07-19 00:35:41 +0900

    feat: 깃허브를 통한 로그인 API 구현

--- backend/src/test/java/com/woowacourse/gongcheck/documentation/DocumentationTest.java
+++ backend/src/test/java/com/woowacourse/gongcheck/documentation/DocumentationTest.java
@@ -14,6 +14,7 @@ import com.woowacourse.gongcheck.application.SubmissionService;
 import com.woowacourse.gongcheck.application.TaskService;
 import com.woowacourse.gongcheck.presentation.AuthenticationContext;
 import com.woowacourse.gongcheck.presentation.GuestAuthController;
+import com.woowacourse.gongcheck.presentation.HostAuthController;
 import com.woowacourse.gongcheck.presentation.HostController;
 import com.woowacourse.gongcheck.presentation.JobController;
 import com.woowacourse.gongcheck.presentation.SpaceController;
@@ -31,6 +32,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @WebMvcTest({
+        HostAuthController.class,
         GuestAuthController.class,
         SpaceController.class,
         JobController.class,
