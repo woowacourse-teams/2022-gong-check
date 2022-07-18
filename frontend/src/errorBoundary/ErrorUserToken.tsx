98202a412f0b3439f318c068cfddafcd428be9a4
intae92 <56149367+intae92@users.noreply.github.com>
2022-07-18 14:31:13 +0900

    [FE] 사용자, 토큰 만료 시, 에러처리를 해야한다. (#165)
    * feat: 사용자의 토큰 관련 error boundary 구현
    
    * refactor: UserToken error boundary 분리
    
    * refactor: 에러 분기처리 로직 수정

---
+++ frontend/src/errorBoundary/ErrorUserToken.tsx
@@ -0,0 +1,39 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import { ErrorBoundary } from 'react-error-boundary';
+import { QueryErrorResetBoundary } from 'react-query';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const EXPIRED_TOKEN_TEXT = '만료된 토큰입니다.';
+const NOT_TOKEN_TEXT = '헤더에 토큰 값이 정상적으로 존재하지 않습니다.';
+
+interface ErrorUserTokenProps {
+  children: React.ReactNode;
+}
+
+const ErrorUserToken: React.FC<ErrorUserTokenProps> = ({ children }) => {
+  const navigate = useNavigate();
+  const { hostId } = useParams();
+
+  return (
+    <QueryErrorResetBoundary>
+      <ErrorBoundary
+        fallbackRender={({ error }) => {
+          const err = error as AxiosError;
+          const res = err.response as AxiosResponse;
+          const message = res.data.message;
+
+          if (message === EXPIRED_TOKEN_TEXT || message === NOT_TOKEN_TEXT) {
+            localStorage.removeItem('user');
+            navigate(`/enter/${hostId}/pwd`);
+          }
+
+          return <></>;
+        }}
+      >
+        {children}
+      </ErrorBoundary>
+    </QueryErrorResetBoundary>
+  );
+};
+
+export default ErrorUserToken;
--- frontend/src/layouts/UserLayout/index.tsx
+++ frontend/src/layouts/UserLayout/index.tsx
@@ -1,31 +1,35 @@
+import ErrorUserToken from '@/errorBoundary/ErrorUserToken';
 import { css } from '@emotion/react';
 import { Suspense, useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate, useParams } from 'react-router-dom';
 
 const UserLayout: React.FC = () => {
   const navigate = useNavigate();
+  const { hostId } = useParams();
 
   useEffect(() => {
     if (!localStorage.getItem('user')) {
-      navigate('pwd');
+      navigate(`/enter/${hostId}/pwd`);
     }
   }, []);
 
   return (
-    <div
-      css={css`
-        display: flex;
-        flex-direction: column;
-        width: 400px;
-        min-height: 100vh;
-        background-color: white;
-        position: absolute;
-      `}
-    >
-      <Suspense fallback={<div>로딩 스피너</div>}>
-        <Outlet />
-      </Suspense>
-    </div>
+    <ErrorUserToken>
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          width: 400px;
+          min-height: 100vh;
+          background-color: white;
+          position: absolute;
+        `}
+      >
+        <Suspense fallback={<div>로딩 스피너</div>}>
+          <Outlet />
+        </Suspense>
+      </div>
+    </ErrorUserToken>
   );
 };
 
