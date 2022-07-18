80118ba4baae4c20d4b5420bef159e950f881792
beomWhale <cndqjacndqja@gmail.com>
2022-07-18 23:59:44 +0900

    [BE] host용 Job 생성 API를 구현한다 (#152)
    * feat: host의 작업 생성 기능 Service 구현
    
    * feat: host의 작업 생성 기능 Controller 구현
    
    * refactor: 불필요한 주석 제거
    
    * refactor: 누락된 Transactional 추가
    
    * refactor: Job과 Section에 cascade.persist 추가 수정
    
    * refactor: 한글 이름의 도메인 용어 영어로 수정
    
    * test: Job 생성 시, 예외 테스트 추가
    
    * test: 불필요한 테스트 제거
    
    * refactor: JobService createSection 로직 변경
    
    * refactor: SectionRequest, TaskRequest NotNull 추가 수정
    
    * test: Job 생성 테스트 세부 검증 추가
    
    * refactor: JobService 메서드 람다 변수명 수정
    
    * refactor: JobAcceptanceTest 피드백 반영
    
    * refactor: Section과 Task에 cascade 옵션 제거 수정
    
    * refactor: 테스트에 도메인 용어 영어로 수정
    
    * refactor: SectionCreateRequest, TaskCreateRequest 클래스명 수정

--- backend/src/main/java/com/woowacourse/gongcheck/application/JobService.java
+++ backend/src/main/java/com/woowacourse/gongcheck/application/JobService.java
@@ -5,8 +5,18 @@ import com.woowacourse.gongcheck.domain.host.Host;
 import com.woowacourse.gongcheck.domain.host.HostRepository;
 import com.woowacourse.gongcheck.domain.job.Job;
 import com.woowacourse.gongcheck.domain.job.JobRepository;
+import com.woowacourse.gongcheck.domain.section.Section;
+import com.woowacourse.gongcheck.domain.section.SectionRepository;
 import com.woowacourse.gongcheck.domain.space.Space;
 import com.woowacourse.gongcheck.domain.space.SpaceRepository;
+import com.woowacourse.gongcheck.domain.task.Task;
+import com.woowacourse.gongcheck.domain.task.TaskRepository;
+import com.woowacourse.gongcheck.presentation.request.JobCreateRequest;
+import com.woowacourse.gongcheck.presentation.request.SectionCreateRequest;
+import com.woowacourse.gongcheck.presentation.request.TaskCreateRequest;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
@@ -19,12 +29,17 @@ public class JobService {
     private final HostRepository hostRepository;
     private final SpaceRepository spaceRepository;
     private final JobRepository jobRepository;
+    private final SectionRepository sectionRepository;
+    private final TaskRepository taskRepository;
 
     public JobService(final HostRepository hostRepository, final SpaceRepository spaceRepository,
-                      final JobRepository jobRepository) {
+                      final JobRepository jobRepository, final SectionRepository sectionRepository,
+                      final TaskRepository taskRepository) {
         this.hostRepository = hostRepository;
         this.spaceRepository = spaceRepository;
         this.jobRepository = jobRepository;
+        this.sectionRepository = sectionRepository;
+        this.taskRepository = taskRepository;
     }
 
     public JobsResponse findPage(final Long hostId, final Long spaceId, final Pageable pageable) {
@@ -32,5 +47,51 @@ public class JobService {
         Space space = spaceRepository.getByHostAndId(host, spaceId);
         Slice<Job> jobs = jobRepository.findBySpaceHostAndSpace(host, space, pageable);
         return JobsResponse.from(jobs);
+    }
+
+    @Transactional
+    public Long createJob(final Long hostId, final Long spaceId, final JobCreateRequest request) {
+        Host host = hostRepository.getById(hostId);
+        Space space = spaceRepository.getByHostAndId(host, spaceId);
+
+        Job job = Job.builder()
+                .space(space)
+                .name(request.getName())
+                .createdAt(LocalDateTime.now())
+                .build();
+        jobRepository.save(job);
+        createSectionsAndTasks(request.getSections(), job);
+        return job.getId();
+    }
+
+    private void createSectionsAndTasks(final List<SectionCreateRequest> sectionCreateRequests, final Job job) {
+        sectionCreateRequests.forEach(sectionRequest -> createSectionAndTasks(sectionRequest, job));
+    }
+
+    private Section createSectionAndTasks(final SectionCreateRequest sectionCreateRequest, final Job job) {
+        Section section = Section.builder()
+                .job(job)
+                .name(sectionCreateRequest.getName())
+                .createdAt(LocalDateTime.now())
+                .build();
+        sectionRepository.save(section);
+        createTasks(sectionCreateRequest.getTasks(), section);
+        return section;
+    }
+
+    private void createTasks(final List<TaskCreateRequest> taskCreateRequests, final Section section) {
+        List<Task> tasks = taskCreateRequests
+                .stream()
+                .map(taskRequest -> createTask(taskRequest, section))
+                .collect(Collectors.toList());
+        taskRepository.saveAll(tasks);
+    }
+
+    private Task createTask(final TaskCreateRequest taskCreateRequest, final Section section) {
+        return Task.builder()
+                .section(section)
+                .name(taskCreateRequest.getName())
+                .createdAt(LocalDateTime.now())
+                .build();
     }
 }
