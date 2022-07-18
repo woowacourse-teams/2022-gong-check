cc3c0e05a32074a686a10de6b4f9e7a5ac8345c9
intae92 <kangit2013@gmail.com>
2022-07-18 11:35:13 +0900

    refactor: UserToken error boundary 분리

--- frontend/src/layouts/UserLayout/index.tsx
+++ frontend/src/layouts/UserLayout/index.tsx
@@ -1,12 +1,7 @@
+import ErrorUserToken from '@/errorBoundary/ErrorUserToken';
 import { css } from '@emotion/react';
-import { AxiosResponse, AxiosError } from 'axios';
 import { Suspense, useEffect } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
-import { QueryErrorResetBoundary } from 'react-query';
 import { Outlet, useNavigate, useParams } from 'react-router-dom';
-
-const EXPIRED_TOKEN_TEXT = '만료된 토큰입니다.';
-const NOT_TOKEN_TEXT = '헤더에 토큰 값이 정상적으로 존재하지 않습니다.';
 
 const UserLayout: React.FC = () => {
   const navigate = useNavigate();
@@ -19,41 +14,22 @@ const UserLayout: React.FC = () => {
   }, []);
 
   return (
-    <QueryErrorResetBoundary>
-      <ErrorBoundary
-        fallbackRender={({ error }) => {
-          const err = error as AxiosError;
-          const res = err.response as AxiosResponse;
-          const message = res.data.message;
-
-          if (message === EXPIRED_TOKEN_TEXT) {
-            localStorage.removeItem('user');
-            navigate(`/enter/${hostId}/pwd`);
-          }
-
-          if (message === NOT_TOKEN_TEXT) {
-            navigate(`/enter/${hostId}/pwd`);
-          }
-
-          return <></>;
-        }}
+    <ErrorUserToken>
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          width: 400px;
+          min-height: 100vh;
+          background-color: white;
+          position: absolute;
+        `}
       >
-        <div
-          css={css`
-            display: flex;
-            flex-direction: column;
-            width: 400px;
-            min-height: 100vh;
-            background-color: white;
-            position: absolute;
-          `}
-        >
-          <Suspense fallback={<div>로딩 스피너</div>}>
-            <Outlet />
-          </Suspense>
-        </div>
-      </ErrorBoundary>
-    </QueryErrorResetBoundary>
+        <Suspense fallback={<div>로딩 스피너</div>}>
+          <Outlet />
+        </Suspense>
+      </div>
+    </ErrorUserToken>
   );
 };
 
