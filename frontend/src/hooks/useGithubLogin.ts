07303642a3a8025b0e3754152e8a380da89d3043
cks3066 <cks3066@naver.com>
2022-07-19 18:36:22 +0900

    feat: 깃허브 Oauth 리다이렉션 처리

--- frontend/src/hooks/useGithubLogin.ts
+++ frontend/src/hooks/useGithubLogin.ts
@@ -1,31 +1,22 @@
 import { useEffect } from 'react';
 import { useQuery } from 'react-query';
-import { useNavigate } from 'react-router-dom';
 
-import apis from '@/apis';
-import githubAuth from '@/apis/githubAuth';
+import apiAuth from '@/apis/githubAuth';
 
 const useGitHubLogin = () => {
   const code = new URL(location.href).searchParams.get('code');
 
-  const navigate = useNavigate();
-
-  const { data } = useQuery(['hostToken'], () => githubAuth.getToken(code), { suspense: true });
-  const { data: spacesData } = useQuery(['spaces'], apis.getSpaces, { suspense: true });
+  const { isSuccess: isSuccessGithubLogin, data } = useQuery(['hostToken'], () => apiAuth.getToken(code), {
+    suspense: true,
+  });
 
   useEffect(() => {
     if (data) {
       localStorage.setItem('token', data.token);
-
-      if (spacesData?.spaces.length === 0) {
-        navigate('/host/manage/spaceCreate');
-        return;
-      }
-
-      const space = spacesData?.spaces[0];
-      navigate(`/host/manage/${space?.id}`);
     }
-  }, [data, spacesData]);
+  }, [data]);
+
+  return { isSuccessGithubLogin };
 };
 
 export default useGitHubLogin;
--- frontend/src/pages/host/AuthCallBack/index.tsx
+++ frontend/src/pages/host/AuthCallBack/index.tsx
@@ -1,9 +1,39 @@
 import { css } from '@emotion/react';
+import { useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 
 import useGitHubLogin from '@/hooks/useGithubLogin';
 
+import apis from '@/apis';
+
 const AuthCallBack: React.FC = () => {
-  useGitHubLogin();
+  const navigate = useNavigate();
+
+  const { isSuccessGithubLogin } = useGitHubLogin();
+
+  const { data, refetch: getSpaceData } = useQuery(['spaces'], apis.getSpaces, {
+    suspense: true,
+    enabled: false,
+  });
+
+  useEffect(() => {
+    if (isSuccessGithubLogin) {
+      getSpaceData();
+    }
+  }, [isSuccessGithubLogin]);
+
+  useEffect(() => {
+    if (data) {
+      if (data.spaces.length === 0) {
+        navigate('/host/manage/spaceCreate');
+        return;
+      }
+
+      const space = data.spaces[0];
+      navigate(`/host/manage/${space.id}`);
+    }
+  }, [data]);
 
   return (
     <div
