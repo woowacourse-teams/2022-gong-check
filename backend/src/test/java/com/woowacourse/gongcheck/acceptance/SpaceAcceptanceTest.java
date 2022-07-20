50c6d7d475f6800c8a13ece2409b043c76a5b6b8
meatsby <meatsby@gmail.com>
2022-07-20 20:34:57 +0900

    test: host 토큰과 guest 토큰 space 인수테스트 수정

--- backend/src/test/java/com/woowacourse/gongcheck/acceptance/SpaceAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/gongcheck/acceptance/SpaceAcceptanceTest.java
@@ -1,12 +1,11 @@
 package com.woowacourse.gongcheck.acceptance;
 
+import static com.woowacourse.gongcheck.acceptance.AuthSupport.Host_토큰을_요청한다;
 import static com.woowacourse.gongcheck.acceptance.AuthSupport.토큰을_요청한다;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import com.woowacourse.gongcheck.presentation.request.GuestEnterRequest;
+import com.woowacourse.gongcheck.presentation.request.TokenRequest;
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import java.io.File;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
@@ -16,48 +15,23 @@ import org.springframework.http.MediaType;
 class SpaceAcceptanceTest extends AcceptanceTest {
 
     @Test
-    void Space를_조회한다() {
-        GuestEnterRequest guestEnterRequest = new GuestEnterRequest("1234");
-        String token = 토큰을_요청한다(guestEnterRequest);
+    void Host_토큰으로_Space를_조회한다() {
+        String token = Host_토큰을_요청한다(new TokenRequest("code")).getToken();
 
-        ExtractableResponse<Response> response = RestAssured
+        RestAssured
                 .given().log().all()
                 .auth().oauth2(token)
                 .when().get("/api/spaces?page=0&size=5")
                 .then().log().all()
-                .extract();
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+                .statusCode(HttpStatus.OK.value());
     }
 
     @Test
-    void Space를_생성한다() throws IOException {
+    void Host_토큰으로_Space를_생성한다() throws IOException {
         File fakeImage = File.createTempFile("temp", ".jpg");
 
-        // 호스트 로그인 구현 전까지 토큰 입력용으로 사용
-        GuestEnterRequest guestEnterRequest = new GuestEnterRequest("1234");
-        String token = 토큰을_요청한다(guestEnterRequest);
+        String token = Host_토큰을_요청한다(new TokenRequest("code")).getToken();
 
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
-                .multiPart("name", "잠실 캠퍼스")
-                .multiPart("image", fakeImage)
-                .auth().oauth2(token)
-                .when().post("/api/spaces")
-                .then().log().all()
-                .extract();
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    @Test
-    void 한_Host가_이미_존재하는_이름의_Space를_생성하면_에러_응답을_반환한다() throws IOException {
-        File fakeImage = File.createTempFile("temp", ".jpg");
-
-        // 호스트 로그인 구현 전까지 토큰 입력용으로 사용
-        GuestEnterRequest guestEnterRequest = new GuestEnterRequest("1234");
-        String token = 토큰을_요청한다(guestEnterRequest);
         RestAssured
                 .given().log().all()
                 .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -66,9 +40,25 @@ class SpaceAcceptanceTest extends AcceptanceTest {
                 .auth().oauth2(token)
                 .when().post("/api/spaces")
                 .then().log().all()
-                .extract();
+                .statusCode(HttpStatus.CREATED.value());
+    }
 
-        ExtractableResponse<Response> response = RestAssured
+    @Test
+    void Host_토큰으로_한_Host가_이미_존재하는_이름의_Space를_생성하면_에러_응답을_반환한다() throws IOException {
+        File fakeImage = File.createTempFile("temp", ".jpg");
+
+        String token = Host_토큰을_요청한다(new TokenRequest("code")).getToken();
+
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                .multiPart("name", "잠실 캠퍼스")
+                .multiPart("image", fakeImage)
+                .auth().oauth2(token)
+                .when().post("/api/spaces")
+                .then().log().all();
+
+        RestAssured
                 .given().log().all()
                 .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
                 .multiPart("name", "잠실 캠퍼스")
@@ -76,40 +66,83 @@ class SpaceAcceptanceTest extends AcceptanceTest {
                 .auth().oauth2(token)
                 .when().post("/api/spaces")
                 .then().log().all()
-                .extract();
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
     @Test
-    void 단일_Space를_조회한다() {
-        // 호스트 로그인 구현 전까지 토큰 입력용으로 사용
-        GuestEnterRequest guestEnterRequest = new GuestEnterRequest("1234");
-        String token = 토큰을_요청한다(guestEnterRequest);
+    void Host_토큰으로_단일_Space를_조회한다() {
+        String token = Host_토큰을_요청한다(new TokenRequest("code")).getToken();
 
-        ExtractableResponse<Response> response = RestAssured
+        RestAssured
                 .given().log().all()
                 .auth().oauth2(token)
                 .when().get("/api/spaces/1")
                 .then().log().all()
-                .extract();
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+                .statusCode(HttpStatus.OK.value());
     }
 
     @Test
-    void Space를_삭제한다() {
-        // 호스트 로그인 구현 전까지 토큰 입력용으로 사용
-        GuestEnterRequest guestEnterRequest = new GuestEnterRequest("1234");
-        String token = 토큰을_요청한다(guestEnterRequest);
+    void Host_토큰으로_Space를_삭제한다() {
+        String token = Host_토큰을_요청한다(new TokenRequest("code")).getToken();
 
-        ExtractableResponse<Response> response = RestAssured
+        RestAssured
                 .given().log().all()
                 .auth().oauth2(token)
                 .when().delete("/api/spaces/1")
                 .then().log().all()
-                .extract();
+                .statusCode(HttpStatus.NO_CONTENT.value());
+    }
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    @Test
+    void Guest_토큰으로_Space를_조회한다() {
+        String token = 토큰을_요청한다(new GuestEnterRequest("1234"));
+
+        RestAssured
+                .given().log().all()
+                .auth().oauth2(token)
+                .when().get("/api/spaces?page=0&size=5")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void Guest_토큰으로_Space를_생성한다() throws IOException {
+        File fakeImage = File.createTempFile("temp", ".jpg");
+
+        String token = 토큰을_요청한다(new GuestEnterRequest("1234"));
+
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                .multiPart("name", "잠실 캠퍼스")
+                .multiPart("image", fakeImage)
+                .auth().oauth2(token)
+                .when().post("/api/spaces")
+                .then().log().all()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void Guest_토큰으로_단일_Space를_조회한다() {
+        String token = 토큰을_요청한다(new GuestEnterRequest("1234"));
+
+        RestAssured
+                .given().log().all()
+                .auth().oauth2(token)
+                .when().get("/api/spaces/1")
+                .then().log().all()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void Guest_토큰으로_Space를_삭제한다() {
+        String token = 토큰을_요청한다(new GuestEnterRequest("1234"));
+
+        RestAssured
+                .given().log().all()
+                .auth().oauth2(token)
+                .when().delete("/api/spaces/1")
+                .then().log().all()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
     }
 }
