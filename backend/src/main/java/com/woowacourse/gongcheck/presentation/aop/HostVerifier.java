29665254c5b817bf8366f0ed6f89771779155f84
meatsby <meatsby@gmail.com>
2022-07-20 19:21:22 +0900

    feat: Host AOP 애너테이션 구현

---
+++ backend/src/main/java/com/woowacourse/gongcheck/presentation/aop/HostVerifier.java
@@ -0,0 +1,27 @@
+package com.woowacourse.gongcheck.presentation.aop;
+
+import com.woowacourse.gongcheck.exception.UnauthorizedException;
+import com.woowacourse.gongcheck.presentation.AuthenticationContext;
+import com.woowacourse.gongcheck.presentation.Authority;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class HostVerifier {
+
+    private final AuthenticationContext authenticationContext;
+
+    public HostVerifier(final AuthenticationContext authenticationContext) {
+        this.authenticationContext = authenticationContext;
+    }
+
+    @Before("@annotation(com.woowacourse.gongcheck.presentation.aop.HostOnly)")
+    public void checkHost() {
+        final Authority authority = authenticationContext.getAuthority();
+        if (!authority.equals(Authority.HOST)) {
+            throw new UnauthorizedException("호스트만 입장 가능합니다.");
+        }
+    }
+}
