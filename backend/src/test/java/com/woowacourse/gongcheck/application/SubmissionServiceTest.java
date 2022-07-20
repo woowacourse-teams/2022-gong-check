445ccdc0175d56f8f6d6ef75bfbd02fc521a8234
awesomeo184 <awesomeo184@gmail.com>
2022-07-20 14:31:50 +0900

    test: HostId와 SpaceId가 연관이 없는 경우의 예외 추가

--- backend/src/test/java/com/woowacourse/gongcheck/application/SubmissionServiceTest.java
+++ backend/src/test/java/com/woowacourse/gongcheck/application/SubmissionServiceTest.java
@@ -155,13 +155,15 @@ class SubmissionServiceTest {
     @Nested
     class Submission_목록을_응답한다 {
         private Host host;
+        private Host anotherHost;
         private Space space;
         private Job job1;
         private Job job2;
 
         @BeforeEach
         void setUp() {
-            host = hostRepository.save(Host_생성("1234", 1234L));
+            host = hostRepository.save(Host_생성("1234", 1111L));
+            anotherHost = hostRepository.save(Host_생성("1234", 2222L));
             space = spaceRepository.save(Space_생성(host, "잠실"));
             job1 = jobRepository.save(Job_생성(space, "오픈"));
             job2 = jobRepository.save(Job_생성(space, "마감"));
@@ -184,6 +186,16 @@ class SubmissionServiceTest {
             Long hostId = host.getId();
             PageRequest pageRequest = PageRequest.of(0, 2);
             assertThatThrownBy(() -> submissionService.findPage(hostId, 0L, pageRequest))
+                    .isInstanceOf(NotFoundException.class)
+                    .hasMessage("존재하지 않는 공간입니다.");
+        }
+
+        @Test
+        void 입력된_SpaceId가_입력된_Host의_Space가_아닌_경우_예외를_발생시킨다() {
+            Long anotherHostId = anotherHost.getId();
+            Long spaceId = space.getId();
+            PageRequest pageRequest = PageRequest.of(0, 2);
+            assertThatThrownBy(() -> submissionService.findPage(anotherHostId, spaceId, pageRequest))
                     .isInstanceOf(NotFoundException.class)
                     .hasMessage("존재하지 않는 공간입니다.");
         }
