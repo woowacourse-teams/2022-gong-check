37e031d65f943a268ea02a2a7dd8f822ea291fb0
intae92 <56149367+intae92@users.noreply.github.com>
2022-07-21 18:00:30 +0900

    [FE] 사용자, 페이지 이동 시, 다음 페이지에 보여질 데이터가 없으면 페이지 이동을 막아야 한다. (#259)
    fix: 사용자 job(체크리스트)가 없으면 페이지 이동 막기

--- frontend/src/ErrorBoundary/ErrorUserToken.tsx
+++ frontend/src/ErrorBoundary/ErrorUserToken.tsx
@@ -1,7 +1,10 @@
 import { AxiosError } from 'axios';
+import { useEffect, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { QueryErrorResetBoundary } from 'react-query';
 import { useNavigate, useParams } from 'react-router-dom';
+
+import useToast from '@/hooks/useToast';
 
 const EXPIRED_TOKEN_TEXT = '만료된 토큰입니다.';
 const NOT_TOKEN_TEXT = '헤더에 토큰 값이 정상적으로 존재하지 않습니다.';
@@ -12,7 +15,21 @@ interface ErrorUserTokenProps {
 
 const ErrorUserToken: React.FC<ErrorUserTokenProps> = ({ children }) => {
   const navigate = useNavigate();
-  const { hostId } = useParams();
+  const { hostId, spaceId } = useParams();
+  const [message, setMessage] = useState<string | undefined>('');
+  const { openToast } = useToast();
+
+  useEffect(() => {
+    if (message === EXPIRED_TOKEN_TEXT || message === NOT_TOKEN_TEXT) {
+      localStorage.removeItem('token');
+      navigate(`/enter/${hostId}/pwd`);
+    }
+
+    if (message === '현재 진행중인 작업이 존재하지 않아 조회할 수 없습니다') {
+      navigate(`/enter/${hostId}/spaces/${spaceId}`);
+      openToast('ERROR', message);
+    }
+  }, [message]);
 
   return (
     <QueryErrorResetBoundary>
@@ -21,10 +38,7 @@ const ErrorUserToken: React.FC<ErrorUserTokenProps> = ({ children }) => {
           const err = error as AxiosError<{ message: string }>;
           const message = err.response?.data.message;
 
-          if (message === EXPIRED_TOKEN_TEXT || message === NOT_TOKEN_TEXT) {
-            localStorage.removeItem('token');
-            navigate(`/enter/${hostId}/pwd`);
-          }
+          setMessage(message);
 
           return <></>;
         }}
