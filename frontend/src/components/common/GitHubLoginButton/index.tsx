63c162e2c40a5cb8cbccc75bbcea081442c5beef
cks3066 <cks3066@naver.com>
2022-07-27 02:38:01 +0900

    refactor: 변수 기본 할당값 조정

--- frontend/src/components/common/GitHubLoginButton/index.tsx
+++ frontend/src/components/common/GitHubLoginButton/index.tsx
@@ -5,7 +5,7 @@ import styles from './styles';
 const GITHUB_LOGIN_URL_PROD = 'https://github.com/login/oauth/authorize?client_id=e78f7565dee502d18ca2';
 const GITHUB_LOGIN_URL_DEV = 'https://github.com/login/oauth/authorize?client_id=964df4781c35de18694a';
 
-let GITHUB_LOGIN_URL;
+let GITHUB_LOGIN_URL = '';
 
 const GitHubLoginButton = () => {
   process.env.NODE_ENV === 'development'
