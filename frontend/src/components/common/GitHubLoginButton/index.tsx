9026f4d9202a9dc69572558d11ac01a97be0790b
cks3066 <cks3066@naver.com>
2022-07-27 02:28:33 +0900

    feat: 깃허브 로그인 개발/배포 환경 분리

--- frontend/src/components/common/GitHubLoginButton/index.tsx
+++ frontend/src/components/common/GitHubLoginButton/index.tsx
@@ -2,9 +2,16 @@ import { GoMarkGithub } from 'react-icons/go';
 
 import styles from './styles';
 
-const GITHUB_LOGIN_URL = 'https://github.com/login/oauth/authorize?client_id=e78f7565dee502d18ca2';
+const GITHUB_LOGIN_URL_PROD = 'https://github.com/login/oauth/authorize?client_id=e78f7565dee502d18ca2';
+const GITHUB_LOGIN_URL_DEV = 'https://github.com/login/oauth/authorize?client_id=964df4781c35de18694a';
+
+let GITHUB_LOGIN_URL;
 
 const GitHubLoginButton = () => {
+  process.env.NODE_ENV === 'development'
+    ? (GITHUB_LOGIN_URL = GITHUB_LOGIN_URL_DEV)
+    : (GITHUB_LOGIN_URL = GITHUB_LOGIN_URL_PROD);
+
   return (
     <a href={GITHUB_LOGIN_URL} css={styles.wrapper}>
       <GoMarkGithub size={32} />
