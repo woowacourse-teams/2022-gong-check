767dc22e509ca800f7161513f9e66e4d2a3f3acd
intae92 <kangit2013@gmail.com>
2022-07-27 19:29:21 +0900

    chore: github Oauth URL 주소 설정

--- frontend/src/components/common/GitHubLoginButton/index.tsx
+++ frontend/src/components/common/GitHubLoginButton/index.tsx
@@ -2,14 +2,9 @@ import { GoMarkGithub } from 'react-icons/go';
 
 import styles from './styles';
 
-const GITHUB_LOGIN_URL_PROD = 'https://github.com/login/oauth/authorize?client_id=e78f7565dee502d18ca2';
-const GITHUB_LOGIN_URL_DEV = 'https://github.com/login/oauth/authorize?client_id=964df4781c35de18694a';
-
 const GitHubLoginButton = () => {
-  const isDevelopment = process.env.NODE_ENV === 'development';
-
   return (
-    <a href={isDevelopment ? GITHUB_LOGIN_URL_DEV : GITHUB_LOGIN_URL_PROD} css={styles.wrapper}>
+    <a href={process.env.REACT_APP_GITHUB_LOGIN_URL} css={styles.wrapper}>
       <GoMarkGithub size={32} />
       <p css={styles.text}>GitHub 로그인</p>
     </a>
