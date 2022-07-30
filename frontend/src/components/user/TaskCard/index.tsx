6f77d4e7c78c796c3671c0d18ccceb391a1d7882
intae92 <kangit2013@gmail.com>
2022-07-30 21:40:58 +0900

    feat: task 상세 정보 보기 기능을 구현한다.

--- frontend/src/components/user/TaskCard/index.tsx
+++ frontend/src/components/user/TaskCard/index.tsx
@@ -1,4 +1,9 @@
+import { RiInformationLine } from 'react-icons/ri';
+
 import CheckBox from '@/components/common/Checkbox';
+import DetailedInfoCardModal from '@/components/user/DetailedInfoCardModal';
+
+import useModal from '@/hooks/useModal';
 
 import apis from '@/apis';
 
@@ -12,6 +17,8 @@ type TaskCardProps = {
 };
 
 const TaskCard: React.FC<TaskCardProps> = ({ tasks, getSections }) => {
+  const { openModal } = useModal();
+
   const onClickCheckBox = async (
     e: React.MouseEvent<HTMLElement, MouseEvent> | React.ChangeEvent<HTMLElement>,
     id: ID
@@ -19,6 +26,14 @@ const TaskCard: React.FC<TaskCardProps> = ({ tasks, getSections }) => {
     e.preventDefault();
     await apis.postCheckTask(id);
     getSections();
+  };
+
+  const onClickTaskDetail = (task: any) => {
+    // TODO: api 명세서 나오면 알맞는 데이터를 넣을 생각입니다.
+    const imageUrl = 'https://velog.velcdn.com/images/cks3066/post/258f92c1-32be-4acb-be30-1eb64635c013/image.jpg';
+    const description = '어쩌고 저쩌고저쩌고............ㅇㅇㅇㅇ';
+
+    openModal(<DetailedInfoCardModal name={task.name} imageUrl={imageUrl} description={description} />);
   };
 
   return (
@@ -30,7 +45,10 @@ const TaskCard: React.FC<TaskCardProps> = ({ tasks, getSections }) => {
             checked={task.checked || false}
             id={JSON.stringify(task.id)}
           />
-          <span>{task.name}</span>
+          <div css={styles.textWrapper}>
+            <span>{task.name}</span>
+            <RiInformationLine css={styles.icon} size={20} onClick={() => onClickTaskDetail(task)} />
+          </div>
         </div>
       ))}
     </div>
--- frontend/src/components/user/TaskCard/styles.ts
+++ frontend/src/components/user/TaskCard/styles.ts
@@ -1,10 +1,17 @@
 import { css } from '@emotion/react';
 
+import theme from '@/styles/theme';
+
 const taskCard = css`
   display: flex;
   flex-direction: column;
   width: 320px;
   padding: 16px;
+`;
+
+const textWrapper = css`
+  display: flex;
+  align-items: center;
 
   span {
     font-weight: 500;
@@ -20,6 +27,12 @@ const task = css`
   margin: 18px 0;
 `;
 
-const styles = { taskCard, task };
+const icon = css`
+  color: ${theme.colors.gray500};
+  margin-left: 16px;
+  cursor: pointer;
+`;
+
+const styles = { taskCard, task, icon, textWrapper };
 
 export default styles;
