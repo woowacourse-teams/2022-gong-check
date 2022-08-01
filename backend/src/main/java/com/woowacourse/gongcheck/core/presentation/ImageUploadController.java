f11fb946e86365d035fd5553c06b41e740192b92
최진영 <jinyoungchoi95@gmail.com>
2022-08-01 18:33:28 +0900

    [BE] 이미지 업로드 api를 구현한다. (#348)
    * feat: 이미지 업로드 api 구현 및 테스트
    
    * test: 테스트 클래스 public 접근 제어 수정

---
+++ backend/src/main/java/com/woowacourse/gongcheck/core/presentation/ImageUploadController.java
@@ -0,0 +1,32 @@
+package com.woowacourse.gongcheck.core.presentation;
+
+import com.woowacourse.gongcheck.auth.presentation.AuthenticationPrincipal;
+import com.woowacourse.gongcheck.auth.presentation.aop.HostOnly;
+import com.woowacourse.gongcheck.core.application.ImageUploader;
+import com.woowacourse.gongcheck.core.application.response.ImageUrlResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api")
+public class ImageUploadController {
+
+    private final ImageUploader imageUploader;
+
+    public ImageUploadController(final ImageUploader imageUploader) {
+        this.imageUploader = imageUploader;
+    }
+
+    @PostMapping(value = "/image-upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @HostOnly
+    public ResponseEntity<ImageUrlResponse> uploadImage(@AuthenticationPrincipal final Long hostId,
+                                                        @RequestPart(required = false) final MultipartFile image) {
+        String imageUrl = imageUploader.upload(image, "/images" + "/hostId");
+        return ResponseEntity.ok(ImageUrlResponse.from(imageUrl));
+    }
+}
