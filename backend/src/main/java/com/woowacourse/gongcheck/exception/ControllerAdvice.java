aabda28dd469a0863278383fd37f8bc33f684d4b
meatsby <meatsby@gmail.com>
2022-08-01 21:54:53 +0900

    feat: ControllerAdvice 로깅 추가

--- backend/src/main/java/com/woowacourse/gongcheck/exception/ControllerAdvice.java
+++ backend/src/main/java/com/woowacourse/gongcheck/exception/ControllerAdvice.java
@@ -1,5 +1,9 @@
 package com.woowacourse.gongcheck.exception;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,30 +11,41 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ControllerAdvice {
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorResponse> handleUnauthorized(final RuntimeException e) {
+        log.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(final RuntimeException e) {
+        log.warn(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusiness(final RuntimeException e) {
+        log.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
+        log.warn(e.getMessage());
         return ResponseEntity.badRequest().body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleInternalServerError(RuntimeException e) {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+
+        e.printStackTrace(printWriter);
+        String stackTrace = stringWriter.toString();
+        log.error("Stack Trace : {}", stackTrace);
         return ResponseEntity.internalServerError().body(ErrorResponse.from(e));
     }
 }
