a90026d11f13fded91340f0e8dc9bc75602d5aee
cks3066 <cks3066@naver.com>
2022-08-04 17:23:17 +0900

    fix: 페이지 전환시 모달 자동으로 꺼지도록 수정

--- frontend/src/layouts/DefaultLayout/index.tsx
+++ frontend/src/layouts/DefaultLayout/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import useModal from '@/hooks/useModal';
+
 const DefaultLayout: React.FC = () => {
+  const { closeModal } = useModal();
+
+  useEffect(() => {
+    closeModal();
+  }, []);
+
   return (
     <>
       <Outlet />
