d8451de111bcf7e5186e298b9eda76de7bd21476
intae92 <56149367+intae92@users.noreply.github.com>
2022-08-04 21:12:20 +0900

    [FE] 사용자 체크리스트 progressBar 위에 고정을 시켜야 한다. (#421)
    * desgin: 체크리스트 progressBar를 위에 고정시킨다.
    
    * refactor: 불필요 코드 제거

--- frontend/src/pages/user/TaskList/index.tsx
+++ frontend/src/pages/user/TaskList/index.tsx
@@ -1,5 +1,4 @@
 import useTaskList from './useTaskList';
-import { useEffect } from 'react';
 import { IoIosArrowBack } from 'react-icons/io';
 
 import Button from '@/components/common/Button';
@@ -30,9 +29,11 @@ const TaskList: React.FC = () => {
           <p>{locationState?.jobName}</p>
         </div>
       </div>
-      <div css={styles.progressBarWrapper}>
-        <div css={styles.progressBar(percent || 0)} />
-        <span css={styles.percentText}>{`${checkCount}/${totalCount}`}</span>
+      <div css={styles.progressBarWrapperSticky}>
+        <div css={styles.progressBarWrapper}>
+          <div css={styles.progressBar(percent || 0)} />
+          <span css={styles.percentText}>{`${checkCount}/${totalCount}`}</span>
+        </div>
       </div>
       <div css={styles.contents}>
         <form css={styles.form}>
