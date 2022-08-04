260a53b0f70781f34de27e9a76169d8c104b31cf
awesomeo184 <awesomeo184@gmail.com>
2022-08-04 20:12:02 +0900

    test: equals에 대한 검증 추가

--- backend/src/test/java/com/woowacourse/gongcheck/core/domain/vo/NameTest.java
+++ backend/src/test/java/com/woowacourse/gongcheck/core/domain/vo/NameTest.java
@@ -1,5 +1,6 @@
 package com.woowacourse.gongcheck.core.domain.vo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.woowacourse.gongcheck.exception.BusinessException;
@@ -19,5 +20,21 @@ class NameTest {
         assertThatThrownBy(() -> new Name(""))
                 .isInstanceOf(BusinessException.class)
                 .hasMessage("이름은 공백일 수 없습니다.");
+    }
+
+    @Test
+    void 이름_값이_같으면_같은_객체이다() {
+        Name name1 = new Name("awesome");
+        Name name2 = new Name("awesome");
+
+        assertThat(name1).isEqualTo(name2);
+    }
+
+    @Test
+    void 이름_값이_다르면_다른_객체이다() {
+        Name name1 = new Name("awesome");
+        Name name2 = new Name("notAwesome");
+
+        assertThat(name1).isNotEqualTo(name2);
     }
 }
