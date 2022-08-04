42615bee5ce426438a012a45d3cc576c81166e78
intae92 <56149367+intae92@users.noreply.github.com>
2022-08-04 13:32:34 +0900

    [FE] 서버에서 주는 에러메시지의 text를 정확히 체크 해야 한다. (#402)
    * fix: 에러메시지 체크
    
    * refactor: 코드 수정

--- frontend/src/ErrorBoundary/ErrorUserTask.tsx
+++ frontend/src/ErrorBoundary/ErrorUserTask.tsx
@@ -11,7 +11,7 @@ const NOT_TASK_TEXT = '존재하지 않는 작업입니다.';
 
 // 사용자들이 체크리스트 페이지에 접속 중일 때, 다른 사용자가 체크리시트를 제출 한 경우 발생
 const NOT_JOB_TEXT = '작업이 존재하지 않습니다.';
-const NOT_JOB_WORK = '현재 진행중인 작업이 존재하지 않아 조회할 수 없습니다.';
+const NOT_JOB_WORK_TEXT = '현재 진행중인 작업이 존재하지 않아 조회할 수 없습니다';
 
 interface ErrorUserTaskProps {
   children: React.ReactNode;
@@ -30,7 +30,7 @@ const ErrorUserTask: React.FC<ErrorUserTaskProps> = ({ children }) => {
       navigate(`/enter/${hostId}/spaces`);
     }
 
-    if (message === NOT_JOB_TEXT || message === NOT_JOB_WORK) {
+    if (message === NOT_JOB_TEXT || message === NOT_JOB_WORK_TEXT) {
       openToast('ERROR', `다른 사용자가 체크리시트를 제출 했습니다. 공간 선택 페이지로 이동합니다.`);
       navigate(`/enter/${hostId}/spaces`);
     }
