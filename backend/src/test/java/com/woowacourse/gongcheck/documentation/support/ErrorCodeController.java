09d24f4040adc9e7ca37aa48082c8f30ea7c20bb
meatsby <meatsby@gmail.com>
2022-08-09 23:56:48 +0900

    refactor: 커스텀 snippet 템플릿 수정 및 불필요 클래스 제거

--- backend/src/test/java/com/woowacourse/gongcheck/documentation/support/ErrorCodeController.java
+++ backend/src/test/java/com/woowacourse/gongcheck/documentation/support/ErrorCodeController.java
@@ -1,9 +1,5 @@
 package com.woowacourse.gongcheck.documentation.support;
 
-import com.woowacourse.gongcheck.exception.ErrorCode;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ErrorCodeController {
 
     @GetMapping("/errorCodes")
-    public CustomDocumentationResponse<ErrorCodes> findErrorCodes() {
-        Map<String, String> errorCodes = getDocs(ErrorCode.values());
-        return CustomDocumentationResponse.of(new ErrorCodes(errorCodes));
-    }
-
-    private Map<String, String> getDocs(ErrorCode[] errorCodes) {
-        return Arrays.stream(errorCodes)
-                .collect(Collectors.toMap(ErrorCode::name, ErrorCode::getDescription));
+    public void findErrorCodes() {
     }
 }
--- backend/src/test/java/com/woowacourse/gongcheck/documentation/support/ErrorCodeFieldsSnippet.java
+++ backend/src/test/java/com/woowacourse/gongcheck/documentation/support/ErrorCodeFieldsSnippet.java
@@ -1,31 +1,36 @@
 package com.woowacourse.gongcheck.documentation.support;
 
+import com.woowacourse.gongcheck.exception.ErrorCode;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.operation.Operation;
-import org.springframework.restdocs.payload.AbstractFieldsSnippet;
-import org.springframework.restdocs.payload.FieldDescriptor;
-import org.springframework.restdocs.payload.PayloadSubsectionExtractor;
+import org.springframework.restdocs.snippet.TemplatedSnippet;
 
-public class ErrorCodeFieldsSnippet extends AbstractFieldsSnippet {
+public class ErrorCodeFieldsSnippet extends TemplatedSnippet {
 
-    public ErrorCodeFieldsSnippet(final String name,
-                                  final String type,
-                                  final List<FieldDescriptor> descriptors,
-                                  final Map<String, Object> attributes,
-                                  final boolean ignoreUndocumentedFields,
-                                  final PayloadSubsectionExtractor<?> subsectionExtractor) {
-        super(name, type, descriptors, attributes, ignoreUndocumentedFields, subsectionExtractor);
+    public ErrorCodeFieldsSnippet(final String snippetName, final String templateName) {
+        super(snippetName, templateName, null);
     }
 
     @Override
-    protected MediaType getContentType(final Operation operation) {
-        return operation.getResponse().getHeaders().getContentType();
+    protected Map<String, Object> createModel(final Operation operation) {
+        Map<String, Object> model = new HashMap<>();
+        List<Map<String, Object>> fields = new ArrayList<>();
+        model.put("fields", fields);
+        addErrorCodes(fields);
+        return model;
     }
 
-    @Override
-    protected byte[] getContent(final Operation operation) {
-        return operation.getResponse().getContent();
+    private void addErrorCodes(final List<Map<String, Object>> fields) {
+        for (ErrorCode errorCode : ErrorCode.values()) {
+            Map<String, Object> model = new HashMap<>();
+            model.put("path", errorCode.name());
+            model.put("type", "String");
+            model.put("description", errorCode.getDescription());
+            model.put("optional", false);
+            fields.add(model);
+        }
     }
 }
