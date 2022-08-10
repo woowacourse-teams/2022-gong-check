3deafb53f0c313ba9e3ba18ed94222bb14cc9716
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-10 20:36:14 +0900

    feat: image resize util 생성 및 테스트

---
+++ imagestorage/src/main/java/com/woowacourse/imagestorage/util/ImageResizeUtil.java
@@ -0,0 +1,36 @@
+package com.woowacourse.imagestorage.util;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+public class ImageResizeUtil {
+
+    private ImageResizeUtil() {
+        throw new AssertionError();
+    }
+
+    public static BufferedImage resizedByWidth(final BufferedImage image, final int width) {
+        double resizeRatio = calculateRatio(image.getWidth(), width);
+        int scaledWidth = resizeLengthByRatio(image.getWidth(), resizeRatio);
+        int scaledHeight = resizeLengthByRatio(image.getHeight(), resizeRatio);
+        return resizeImage(image, scaledWidth, scaledHeight);
+    }
+
+    private static double calculateRatio(final int originWitdh, final int changeWidth) {
+        return (double) changeWidth / originWitdh;
+    }
+
+    private static int resizeLengthByRatio(final int length, final double resizeRatio) {
+        return (int) (length * resizeRatio);
+    }
+
+    private static BufferedImage resizeImage(BufferedImage img, int width, int height) {
+        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage resized = new BufferedImage(width, height, img.getType());
+        Graphics2D g2d = resized.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+        return resized;
+    }
+}
