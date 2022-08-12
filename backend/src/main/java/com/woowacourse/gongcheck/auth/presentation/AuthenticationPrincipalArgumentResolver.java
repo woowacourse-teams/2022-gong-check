26a5b979b1129c1c37ba1c79a692e0d11f715755
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-12 10:53:36 +0900

    refactor: throws exception 제거

--- backend/src/main/java/com/woowacourse/gongcheck/auth/presentation/AuthenticationPrincipalArgumentResolver.java
+++ backend/src/main/java/com/woowacourse/gongcheck/auth/presentation/AuthenticationPrincipalArgumentResolver.java
@@ -24,8 +24,7 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
 
     @Override
     public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
-                                  final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory)
-            throws Exception {
+                                  final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) {
         return Long.valueOf(authenticationContext.getPrincipal());
     }
 }
