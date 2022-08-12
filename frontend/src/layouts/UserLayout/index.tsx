c3c7da3b778dc50c3675d4519d36fa8e0c924726
cks3066 <cks3066@naver.com>
2022-08-12 11:31:13 +0900

    feat: 레이아웃에서 HOST ID 비교

--- frontend/src/layouts/UserLayout/index.tsx
+++ frontend/src/layouts/UserLayout/index.tsx
@@ -2,7 +2,10 @@ import ErrorUserTask from '@/ErrorBoundary/ErrorUserTask';
 import ErrorUserToken from '@/ErrorBoundary/ErrorUserToken';
 import { Global } from '@emotion/react';
 import { Suspense, useEffect } from 'react';
+import { useQuery } from 'react-query';
 import { Outlet, useNavigate, useParams } from 'react-router-dom';
+
+import apiHost from '@/apis/host';
 
 import { ID } from '@/types';
 
@@ -15,8 +18,10 @@ const UserLayout: React.FC = () => {
 
   const { hostId } = useParams() as { hostId: ID };
 
+  const { data: entranceCodeData } = useQuery(['hostId', hostId], apiHost.getEntranceCode);
+
   useEffect(() => {
-    if (!localStorage.getItem('token')) {
+    if (!localStorage.getItem('token') || entranceCodeData?.entranceCode !== hostId) {
       navigate(`/enter/${hostId}/pwd`);
     }
   }, []);
