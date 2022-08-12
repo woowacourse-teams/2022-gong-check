be4219907a3a549af72e5341e0eeb8ac3a6c2345
cks3066 <cks3066@naver.com>
2022-08-12 16:01:20 +0900

    feat: TaskList 이미지 페이지 레이지로딩 적용

--- frontend/src/components/user/SectionInfoPreview/index.tsx
+++ frontend/src/components/user/SectionInfoPreview/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import { FaMapMarkedAlt } from 'react-icons/fa';
 
 import styles from './styles';
@@ -7,11 +8,38 @@ interface SectionInfoPreviewProps {
   onClick: () => void;
 }
 
+const useLazyImage = () => {
+  const lazyImageRef = useRef<HTMLImageElement>(null);
+  const observerRef = useRef<IntersectionObserver>();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const intersectionCallBack = (entries: IntersectionObserverEntry[], io: IntersectionObserver) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        io.unobserve(entry.target);
+        setIsLoaded(true);
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!observerRef.current) {
+      observerRef.current = new IntersectionObserver(intersectionCallBack);
+    }
+
+    lazyImageRef.current && observerRef.current.observe(lazyImageRef.current);
+  }, []);
+
+  return { isLoaded, lazyImageRef };
+};
+
 const SectionInfoPreview: React.FC<SectionInfoPreviewProps> = ({ imageUrl, onClick }) => {
+  const { isLoaded, lazyImageRef } = useLazyImage();
+
   return (
     <div css={styles.wrapper} onClick={onClick}>
       <div css={styles.imageWrapper}>
-        <img css={styles.image} src={imageUrl} />
+        <img css={styles.image} src={isLoaded ? imageUrl : ''} ref={lazyImageRef} />
       </div>
       <FaMapMarkedAlt css={styles.icon} size={24} />
     </div>
