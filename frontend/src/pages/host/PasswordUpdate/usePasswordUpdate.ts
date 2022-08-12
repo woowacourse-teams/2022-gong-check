09ca551f5847dd0081753316f535434a6e74d0a3
cks3066 <cks3066@naver.com>
2022-08-12 13:23:33 +0900

    feat: 입장코드 변경시 뒤로가는 기능 추가

--- frontend/src/pages/host/PasswordUpdate/usePasswordUpdate.ts
+++ frontend/src/pages/host/PasswordUpdate/usePasswordUpdate.ts
@@ -1,12 +1,15 @@
 import { AxiosError } from 'axios';
 import { useState } from 'react';
 import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 
 import useToast from '@/hooks/useToast';
 
 import apiPassword from '@/apis/password';
 
 const usePasswordUpdate = () => {
+  const navigate = useNavigate();
+
   const { openToast } = useToast();
 
   const [password, setPassword] = useState('');
@@ -15,6 +18,7 @@ const usePasswordUpdate = () => {
   const { mutate: updatePassword } = useMutation(() => apiPassword.patchSpacePassword(password), {
     onSuccess: () => {
       openToast('SUCCESS', '비밀번호 변경에 성공하였습니다.');
+      navigate(-1);
     },
     onError: (err: AxiosError<{ message: string }>) => {
       openToast('ERROR', `${err.response?.data.message}`);
