7b3a634bc9f11b82a573e8e60c0ee4a57a1fe121
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-13 17:50:57 +0900

    feat: resize 이미지 반환 controller 추가

--- imagestorage/src/main/java/com/woowacourse/imagestorage/presentation/ImageController.java
+++ imagestorage/src/main/java/com/woowacourse/imagestorage/presentation/ImageController.java
@@ -2,14 +2,23 @@ package com.woowacourse.imagestorage.presentation;
 
 import com.woowacourse.imagestorage.application.ImageService;
 import com.woowacourse.imagestorage.application.response.ImageResponse;
+import java.util.concurrent.TimeUnit;
+import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ImageController {
+
+    private static final String DEFAULT_RESIZE_WIDTH = "500";
+    private static final int CACHE_CONTROL_MAX_AGE = 30;
 
     private final ImageService imageService;
 
@@ -22,5 +31,13 @@ public class ImageController {
 
         ImageResponse imageResponse = imageService.storeImage(file);
         return ResponseEntity.ok(imageResponse.getImagePath());
+    }
+
+    @GetMapping(value = "/api/resize/{imageUrl}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getResizeImage(@PathVariable String imageUrl,
+                                                 @RequestParam(required = false, defaultValue = DEFAULT_RESIZE_WIDTH) int width) {
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(CACHE_CONTROL_MAX_AGE, TimeUnit.DAYS))
+                .body(imageService.resizeImage(imageUrl, width));
     }
 }
