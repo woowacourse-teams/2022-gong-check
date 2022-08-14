8318745b7766d15767383247fd4300fc6c331082
Seungchan On <62434898+cks3066@users.noreply.github.com>
2022-08-14 13:18:29 +0900

    [FE] 작업 체크 페이지에 이미지 LazyLoading을 적용한다. (#487)
    * feat: TaskList 이미지 페이지 레이지로딩 적용
    
    * refactor: 훅 분리

--- frontend/src/components/user/SectionInfoPreview/index.tsx
+++ frontend/src/components/user/SectionInfoPreview/index.tsx
@@ -1,5 +1,7 @@
 import { FaMapMarkedAlt } from 'react-icons/fa';
 
+import useLazyImage from '@/hooks/useLazyImage';
+
 import styles from './styles';
 
 interface SectionInfoPreviewProps {
@@ -8,10 +10,12 @@ interface SectionInfoPreviewProps {
 }
 
 const SectionInfoPreview: React.FC<SectionInfoPreviewProps> = ({ imageUrl, onClick }) => {
+  const { isLoaded, lazyImageRef } = useLazyImage();
+
   return (
     <div css={styles.wrapper} onClick={onClick}>
       <div css={styles.imageWrapper}>
-        <img css={styles.image} src={imageUrl} />
+        <img css={styles.image} src={isLoaded ? imageUrl : ''} ref={lazyImageRef} />
       </div>
       <FaMapMarkedAlt css={styles.icon} size={24} />
     </div>
