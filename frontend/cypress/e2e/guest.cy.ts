e235413ad3149c866b324b0fc54f5202d854764b
cks3066 <cks3066@naver.com>
2022-08-16 20:53:37 +0900

    fix: 테스트코드 실행 순서 보장을 위해 then으로 순서 명시

--- frontend/cypress/e2e/guest.cy.ts
+++ frontend/cypress/e2e/guest.cy.ts
@@ -15,25 +15,28 @@ describe('사용자, - 비밀번호 입력 페이지', () => {
     cy.getSpaces();
   });
 
-  it('사용자가 잘못된 비밀번호를 입력하면, API 호출이 실패하고 토스트바로 안내해준다.', () => {
-    cy.get('input').type(WRONG_PASSWORD);
-
-    cy.get('button').click();
-
-    cy.get('@spaceEnter').then(({ response }: any) => {
-      cy.get('#toast > div').should('be.contain', '비밀번호를 확인해주세요.');
-      expect(response.statusCode).to.equal(401);
-    });
+  it('사용자가 잘못된 비밀번호를 입력하면, 토스트바로 안내해준다.', () => {
+    cy.get('input')
+      .type(WRONG_PASSWORD)
+      .then(() => {
+        cy.get('button')
+          .click()
+          .then(() => {
+            cy.get('#toast > div').should('be.contain', '비밀번호를 확인해주세요.');
+          });
+      });
   });
 
   it('사용자가 올바른 비밀번호를 입력하면, API 호출이 성공하고 공간 선택 페이지로 이동한다.', () => {
-    cy.get('input').type(CORRECT_PASSWORD);
-
-    cy.get('button').click();
-
-    cy.get('@spaceEnter').then(({ response }: any) => {
-      expect(response.statusCode).to.equal(200);
-    });
+    cy.get('input')
+      .type(CORRECT_PASSWORD)
+      .then(() => {
+        cy.get('button')
+          .click()
+          .then(() => {
+            cy.url().should('eq', PAGE.SPACE_LIST);
+          });
+      });
   });
 });
 
@@ -48,9 +51,12 @@ describe('사용자, - 공간 선택 페이지', () => {
   });
 
   it('사용자가 공간을 클릭하면, 공간의 업무 선택 페이지로 이동한다.', () => {
-    cy.get('[class$=-spaceCard]').first().click();
-
-    cy.url().should('eq', PAGE.JOB_LIST);
+    cy.get('[class$=-spaceCard]')
+      .first()
+      .click()
+      .then(() => {
+        cy.url().should('eq', PAGE.JOB_LIST);
+      });
   });
 });
 
@@ -64,37 +70,50 @@ describe('사용자 - 업무 선택 페이지', () => {
   });
 
   it('사용자가 작업이 진행중이지 않은 업무를 클릭하면, confirm 창이 노출된다.', () => {
-    cy.getRunningTaskActive_false(1);
-    cy.postNewRunningTasks(1);
-    cy.get('[class$=-jobCard]').first().click();
-
-    cy.on('window:confirm', text => {
-      expect(text).to.contains('진행중인 체크리스트가 없습니다. 새롭게 생성하시겠습니까?');
-      return false;
+    cy.getRunningTaskActive_false(1).then(() => {
+      cy.postNewRunningTasks(1).then(() => {
+        cy.get('[class$=-jobCard]')
+          .first()
+          .click()
+          .then(() => {
+            cy.on('window:confirm', text => {
+              expect(text).to.contains('진행중인 체크리스트가 없습니다. 새롭게 생성하시겠습니까?');
+              return false;
+            });
+          });
+      });
     });
   });
 
   it('사용자가 작업이 진행중인 업무를 클릭하면, 해당 체크리스트 페이지로 이동한다.', () => {
-    cy.getRunningTaskActive_true(1);
-    cy.postNewRunningTasks(1);
-    cy.get('[class$=-jobCard]').first().click();
-
-    cy.url().should('eq', PAGE.TASK_LIST);
+    cy.getRunningTaskActive_true(1).then(() => {
+      cy.postNewRunningTasks(1).then(() => {
+        cy.get('[class$=-jobCard]')
+          .first()
+          .click()
+          .then(() => {
+            cy.url().should('eq', PAGE.TASK_LIST);
+          });
+      });
+    });
   });
 
   it('사용자가 새로운 체크리스트 생성시, 업무 목록이 존재하지 않으면 토스트바로 안내해준다.', () => {
-    cy.getRunningTaskActive_false(2);
-    cy.postNewRunningTasks(2);
-    cy.get('[class$=-jobCard]').last().click();
-
-    cy.on('window:confirm', text => {
-      expect(text).to.contains('진행중인 체크리스트가 없습니다. 새롭게 생성하시겠습니까?');
-      return true;
-    });
-
-    cy.get('@postNewRunningTasks').then(({ response }: any) => {
-      cy.get('#toast > div').should('be.contain', '작업이 존재하지 않습니다.');
-      expect(response.statusCode).to.equal(401);
+    cy.getRunningTaskActive_false(2).then(() => {
+      cy.postNewRunningTasks(2).then(() => {
+        cy.get('[class$=-jobCard]')
+          .last()
+          .click()
+          .then(() => {
+            cy.on('window:confirm', text => {
+              expect(text).to.contains('진행중인 체크리스트가 없습니다. 새롭게 생성하시겠습니까?');
+              return true;
+            });
+          })
+          .then(() => {
+            cy.get('#toast > div').should('be.contain', '작업이 존재하지 않습니다.');
+          });
+      });
     });
   });
 });
@@ -109,47 +128,87 @@ describe('사용자 - 체크리스트 체크 페이지', () => {
   });
 
   it('사용자가 체크 박스를 클릭하면, 해당 사항이 체크표시된다.', () => {
-    cy.postCheckTask(1);
-    cy.get('label').first().click();
-
-    cy.get('label').first().should('have.css', 'background-color', 'rgb(126, 217, 87)');
+    cy.postCheckTask(1).then(() => {
+      cy.get('label')
+        .first()
+        .click()
+        .then(() => {
+          cy.get('label').first().should('have.css', 'background-color', 'rgb(126, 217, 87)');
+        });
+    });
   });
 
   it('사용자가 모든 체크 박스를 클릭하면, 제출 버튼이 활성화된다.', () => {
-    cy.postCheckTask(1);
-    cy.get('label').first().click();
-    cy.postCheckTask(2);
-    cy.get('label').last().click();
-
-    cy.get('button').should('not.be.disabled');
+    cy.postCheckTask(1).then(() => {
+      cy.get('label')
+        .first()
+        .click()
+        .then(() => {
+          cy.postCheckTask(2).then(() => {
+            cy.get('label')
+              .last()
+              .click()
+              .then(() => {
+                cy.get('button').should('not.be.disabled');
+              });
+          });
+        });
+    });
   });
 
   it('사용자가 제출 버튼을 클릭하면, 제출 모달이 노출된다.', () => {
-    cy.postCheckTask(1);
-    cy.get('label').first().click();
-    cy.postCheckTask(2);
-    cy.get('label').last().click();
-
-    cy.get('button').click();
-
-    cy.get('#modal > div').should('be.visible');
+    cy.postCheckTask(1).then(() => {
+      cy.get('label')
+        .first()
+        .click()
+        .then(() => {
+          cy.postCheckTask(2).then(() => {
+            cy.get('label')
+              .last()
+              .click()
+              .then(() => {
+                cy.get('button')
+                  .click()
+                  .then(() => {
+                    cy.get('#modal > div').should('be.visible');
+                  });
+              });
+          });
+        });
+    });
   });
 
   it('사용자가 제출 모달에서 제출자를 입력하고 버튼을 누르면, 제출 alert가 발생한다.', () => {
-    cy.getSpaces();
-
-    cy.postCheckTask(1);
-    cy.get('label').first().click();
-    cy.postCheckTask(2);
-    cy.get('label').last().click();
-
-    cy.get('button').click();
-
-    cy.get('#modal input').type('coke');
-    cy.get('#modal button').click();
-
-    cy.on('window:alert', text => {
-      expect(text).to.include('제출 되었습니다.');
+    cy.getSpaces().then(() => {
+      cy.postCheckTask(1).then(() => {
+        cy.get('label')
+          .first()
+          .click()
+          .then(() => {
+            cy.postCheckTask(2).then(() => {
+              cy.get('label')
+                .last()
+                .click()
+                .then(() => {
+                  cy.get('button')
+                    .click()
+                    .then(() => {
+                      cy.get('#modal input')
+                        .type('coke')
+                        .then(() => {
+                          cy.get('#modal button')
+                            .click()
+                            .then(() => {
+                              cy.on('window:alert', text => {
+                                expect(text).to.include('제출 되었습니다.');
+                              });
+                            });
+                        });
+                    });
+                });
+            });
+          });
+      });
     });
   });
 });
