718af5977939188d0bdd0abbd8b33c265a89fa4c
awesomeo184 <awesomeo184@gmail.com>
2022-08-17 19:14:56 +0900

    refactor: HostServiceTest 가독성 개선

--- backend/src/test/java/com/woowacourse/gongcheck/core/application/HostServiceTest.java
+++ backend/src/test/java/com/woowacourse/gongcheck/core/application/HostServiceTest.java
@@ -10,7 +10,6 @@ import com.woowacourse.gongcheck.auth.application.EntranceCodeProvider;
 import com.woowacourse.gongcheck.core.domain.host.Host;
 import com.woowacourse.gongcheck.core.presentation.request.SpacePasswordChangeRequest;
 import com.woowacourse.gongcheck.exception.NotFoundException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -32,52 +31,42 @@ class HostServiceTest {
     @Autowired
     private EntranceCodeProvider entranceCodeProvider;
 
+    abstract class Host가_존재한다면 {
+        protected final Host host = repository.save(Host_생성("1234", 1L));
+    }
+
     @Nested
     class changeSpacePassword_메소드는 {
 
         @Nested
-        class 존재하는_Host의_id와_수정할_패스워드를_받는_경우 {
+        class 존재하는_HostId와_수정할_패스워드를_입력받는다면 extends Host가_존재한다면 {
 
-            private static final String ORIGIN_PASSWORD = "1234";
             private static final String CHANGING_PASSWORD = "4567";
-            private static final long GITHUB_ID = 1234L;
 
-            private SpacePasswordChangeRequest spacePasswordChangeRequest;
-            private Long hostId;
-
-            @BeforeEach
-            void setUp() {
-                spacePasswordChangeRequest = new SpacePasswordChangeRequest(CHANGING_PASSWORD);
-                hostId = repository.save(Host_생성(ORIGIN_PASSWORD, GITHUB_ID))
-                        .getId();
-            }
+            private final SpacePasswordChangeRequest spacePasswordChangeRequest = new SpacePasswordChangeRequest(
+                    CHANGING_PASSWORD);
 
             @Test
             void 패스워드를_수정한다() {
-                hostService.changeSpacePassword(hostId, spacePasswordChangeRequest);
-                Host actual = repository.getById(Host.class, hostId);
+                hostService.changeSpacePassword(host.getId(), spacePasswordChangeRequest);
+                Host actual = repository.getById(Host.class, host.getId());
 
                 assertThat(actual.getSpacePassword().getValue()).isEqualTo(CHANGING_PASSWORD);
             }
         }
 
         @Nested
-        class 존재하지_않는_Host의_id를_받는_경우 {
+        class 존재하지_않는_HostId를_입력받는다면 {
 
             private static final String CHANGING_PASSWORD = "4567";
+            private static final long NON_EXIST_HOST_ID = 0L;
 
-            private SpacePasswordChangeRequest spacePasswordChangeRequest;
-            private Long hostId;
-
-            @BeforeEach
-            void setUp() {
-                spacePasswordChangeRequest = new SpacePasswordChangeRequest(CHANGING_PASSWORD);
-                hostId = 0L;
-            }
+            private final SpacePasswordChangeRequest spacePasswordChangeRequest = new SpacePasswordChangeRequest(
+                    CHANGING_PASSWORD);
 
             @Test
             void 예외를_발생시킨다() {
-                assertThatThrownBy(() -> hostService.changeSpacePassword(hostId, spacePasswordChangeRequest))
+                assertThatThrownBy(() -> hostService.changeSpacePassword(NON_EXIST_HOST_ID, spacePasswordChangeRequest))
                         .isInstanceOf(NotFoundException.class)
                         .hasMessageContaining("존재하지 않는 호스트입니다.");
             }
@@ -88,31 +77,24 @@ class HostServiceTest {
     class createEntranceCode_메소드는 {
 
         @Nested
-        class 존재하는_Host의_id를_받는_경우 {
+        class 존재하는_HostId를_입력받는다면 extends Host가_존재한다면 {
 
-            private Long hostId;
-            private String expected;
-
-            @BeforeEach
-            void setUp() {
-                hostId = repository.save(Host_생성("1234", 1111L))
-                        .getId();
-                expected = entranceCodeProvider.createEntranceCode(hostId);
-            }
+            private final Long hostId = host.getId();
+            private final String expected = entranceCodeProvider.createEntranceCode(hostId);
 
             @Test
-            void 입장코드를_반환한다() {
+            void entranceCode를_반환한다() {
                 String actual = hostService.createEntranceCode(hostId);
                 assertThat(actual).isEqualTo(expected);
             }
         }
 
         @Nested
-        class 존재하지_않는_Host의_id를_받는_경우 {
+        class 존재하지_않는_HostId를_입력받는다면 {
 
             @Test
             void 예외를_발생시킨다() {
-                assertThatThrownBy(() -> hostService.createEntranceCode(0L))
+                assertThatThrownBy(() -> hostService.createEntranceCode(1L))
                         .isInstanceOf(NotFoundException.class)
                         .hasMessageContaining("존재하지 않는 호스트입니다.");
             }
