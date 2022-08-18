f915b264c60d5544a27094e08419145d5a57e54e
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-18 14:22:24 +0900

    feat: image to webp 수정

--- imagestorage/src/main/java/com/woowacourse/imagestorage/strategy/convert/StaticImg2WebpStrategy.java
+++ imagestorage/src/main/java/com/woowacourse/imagestorage/strategy/convert/StaticImg2WebpStrategy.java
@@ -1,17 +1,17 @@
 package com.woowacourse.imagestorage.strategy.convert;
 
-import com.sksamuel.scrimage.ImmutableImage;
-import com.sksamuel.scrimage.webp.WebpWriter;
 import com.woowacourse.imagestorage.exception.FileConvertException;
+import com.woowacourse.imagestorage.strategy.convert.handler.CwebpHandler;
 import java.io.IOException;
 
 public class StaticImg2WebpStrategy implements Convert2WebpStrategy {
+
+    private static final CwebpHandler DEFAULT = new CwebpHandler();
+
     @Override
     public byte[] convert(final byte[] originBytes) {
         try {
-            return ImmutableImage.loader()
-                    .fromBytes(originBytes)
-                    .bytes(WebpWriter.DEFAULT);
+            return DEFAULT.convert(originBytes);
         } catch (IOException exception) {
             throw new FileConvertException("webp 변환 시 문제가 발생하였습니다.");
         }
---
+++ imagestorage/src/main/java/com/woowacourse/imagestorage/strategy/convert/handler/CwebpHandler.java
@@ -0,0 +1,66 @@
+package com.woowacourse.imagestorage.strategy.convert.handler;
+
+import com.woowacourse.imagestorage.exception.FileIOException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class CwebpHandler extends WebpHandler {
+
+    private static final String CWEBP = "cwebp";
+
+    private static final Path binary;
+
+    static {
+        try {
+            binary = createPlaceholder(CWEBP);
+            installBinary(binary, CWEBP);
+        } catch (IOException exception) {
+            throw new FileIOException("cwebp binary 파일을 읽을 수 없습니다.");
+        }
+    }
+
+    public byte[] convert(byte[] bytes) throws IOException {
+        Path source = Files.createTempFile("input", "gif").toAbsolutePath();
+        Path target = Files.createTempFile("to_webp", "webp").toAbsolutePath();
+        try {
+            Files.write(source, bytes, StandardOpenOption.CREATE);
+            convert(source, target);
+            return Files.readAllBytes(target);
+        } finally {
+            source.toFile()
+                    .delete();
+            target.toFile()
+                    .delete();
+        }
+    }
+
+    private void convert(Path source, Path target) throws IOException {
+        Path stdout = Files.createTempFile("stdout", "webp");
+        List<String> commands = new ArrayList<>();
+        commands.add(binary.toAbsolutePath().toString());
+        commands.add(source.toAbsolutePath().toString());
+        commands.add("-o");
+        commands.add(target.toAbsolutePath().toString());
+
+        ProcessBuilder builder = new ProcessBuilder(commands);
+        builder.redirectErrorStream(true);
+        builder.redirectOutput(stdout.toFile());
+
+        Process process = builder.start();
+        try {
+            process.waitFor(5, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new IOException(e);
+        }
+        int exitStatus = process.exitValue();
+        if (exitStatus != 0) {
+            List<String> error = Files.readAllLines(stdout);
+            throw new IOException(error.toString());
+        }
+    }
+}
