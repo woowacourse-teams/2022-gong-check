7b952cc7f3f08e84d3f0fb48de89d3d0493b5be9
cks3066 <cks3066@naver.com>
2022-09-13 19:27:36 +0900

    feat: joblist 페이지 수정

--- frontend/src/pages/user/JobList/index.tsx
+++ frontend/src/pages/user/JobList/index.tsx
@@ -1,5 +1,5 @@
 import useJobList from './useJobList';
-import { IoIosArrowBack } from 'react-icons/io';
+import { AiOutlineClose } from 'react-icons/ai';
 
 import JobCard from '@/components/user/JobCard';
 
@@ -13,10 +13,9 @@ const JobList: React.FC = () => {
   return (
     <div css={styles.layout}>
       <div css={styles.cover(spaceData?.imageUrl || DEFAULT_IMAGE)}>
-        <IoIosArrowBack css={styles.arrow} size={40} onClick={goPreviousPage} />
+        <AiOutlineClose css={styles.arrow} size={30} onClick={goPreviousPage} />
         <span css={styles.coverText}>{spaceData?.name}</span>
       </div>
-      <span css={styles.text}>체크하실 업무를 선택해주세요.</span>
       {jobsData?.jobs.length === 0 ? (
         <div css={styles.empty}>관리자가 생성한 업무가 없어요</div>
       ) : (
