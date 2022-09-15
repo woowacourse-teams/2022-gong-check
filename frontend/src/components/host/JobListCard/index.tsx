e920b5125c28f211669611ec7459654a92d17319
cks3066 <cks3066@naver.com>
2022-09-15 11:19:36 +0900

    feat: 업무 생성, 수정 컴포넌트 반응형 리팩터링

--- frontend/src/components/host/JobListCard/index.tsx
+++ frontend/src/components/host/JobListCard/index.tsx
@@ -19,7 +19,7 @@ const JobListCard: React.FC<JobListCardProps> = ({ jobs }) => {
   return (
     <div css={styles.layout}>
       <div css={styles.title}>
-        <span>공간 업무 목록</span>
+        <p>업무 목록</p>
         <Button css={styles.newJobButton} onClick={onClickNewJobButton}>
           새 업무 생성
         </Button>
@@ -28,7 +28,7 @@ const JobListCard: React.FC<JobListCardProps> = ({ jobs }) => {
         {jobs.length === 0 ? (
           <div css={styles.empty}>
             <img src={emptyFolder} alt="" />
-            <div>생성된 업무가 없어요.</div>
+            <div>생성된 업무가 없습니다.</div>
           </div>
         ) : (
           jobs.map(job => <JobBox job={job} key={job.id} />)
