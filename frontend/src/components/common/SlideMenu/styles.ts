3972e9678ebfca4fd0fa66fda666c69fa0049d74
cks3066 <cks3066@naver.com>
2022-09-15 14:59:45 +0900

    feat: TopNavigator 수정

--- frontend/src/components/common/SlideMenu/styles.ts
+++ frontend/src/components/common/SlideMenu/styles.ts
@@ -7,9 +7,9 @@ const slideMenu = (isShowMenu: boolean | null) => css`
   position: absolute;
   min-height: 100vh;
   background-color: ${theme.colors.white};
-  z-index: 100;
+  z-index: 1000000;
   width: 100%;
-  box-shadow: 0 -2px 2px 0px ${theme.colors.shadow30};
+
   animation: ${isShowMenu === true ? animation.navigatorOpen : animation.navigatorClose} 0.2s ease-out;
   animation-fill-mode: forwards;
 `;
