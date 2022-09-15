96ff4d92160064f236df401189a3ba8903c87527
cks3066 <cks3066@naver.com>
2022-09-15 11:16:00 +0900

    design: 깃허브 로그인 컴포넌트 디자인 변경

--- frontend/src/components/common/GitHubLoginButton/styles.ts
+++ frontend/src/components/common/GitHubLoginButton/styles.ts
@@ -4,15 +4,16 @@ const wrapper = css`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 260px;
+  width: auto;
   border-radius: 24px;
   color: white;
   background-color: #21262c;
+  padding: 0 16px;
 `;
 
 const text = css`
-  margin-left: 16px;
-  font-size: 24px;
+  margin-left: 12px;
+  font-size: 18px;
 `;
 
 const styles = { wrapper, text };
