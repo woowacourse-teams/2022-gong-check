8e7416ef3242cc4e29ebe488e5e16542d3308704
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-09-25 21:08:40 +0900

    feat: Submission job complete 메소드 진행 시 transaction 전파 수정 (상위단에서 lock 사용시 별도의 transaction에서 동작할 수 있도록 변경)

--- backend/src/main/java/com/woowacourse/gongcheck/core/application/SubmissionService.java
+++ backend/src/main/java/com/woowacourse/gongcheck/core/application/SubmissionService.java
@@ -1,5 +1,7 @@
 package com.woowacourse.gongcheck.core.application;
 
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
+
 import com.woowacourse.gongcheck.core.application.response.SubmissionCreatedResponse;
 import com.woowacourse.gongcheck.core.application.response.SubmissionsResponse;
 import com.woowacourse.gongcheck.core.application.support.LoggingFormatConverter;
@@ -54,7 +56,7 @@ public class SubmissionService {
         this.runningTaskSseEmitterContainer = runningTaskSseEmitterContainer;
     }
 
-    @Transactional
+    @Transactional(propagation = REQUIRES_NEW)
     public void submitJobCompletion(final Long hostId, final Long jobId, final SubmissionRequest request) {
         Host host = hostRepository.getById(hostId);
         Job job = jobRepository.getBySpaceHostAndId(host, jobId);
