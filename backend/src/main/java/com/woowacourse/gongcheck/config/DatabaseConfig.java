ecaf904ec87264ee94de62c4143f1e6fbef5960f
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-09-26 15:57:36 +0900

    feat: 프로덕션 환경 Datasource 설정 추가

--- backend/src/main/java/com/woowacourse/gongcheck/config/DatabaseConfig.java
+++ backend/src/main/java/com/woowacourse/gongcheck/config/DatabaseConfig.java
@@ -1,13 +1,17 @@
 package com.woowacourse.gongcheck.config;
 
+import com.woowacourse.gongcheck.core.domain.lock.JdbcUserLevelLock;
+import com.woowacourse.gongcheck.core.domain.lock.UserLevelLock;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 public class DatabaseConfig {
 
     @Primary
@@ -21,5 +25,10 @@ public class DatabaseConfig {
     @ConfigurationProperties("submission-lock.hikari")
     public HikariDataSource submissionLockDataSource() {
         return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    public UserLevelLock userLevelLock() {
+        return new JdbcUserLevelLock(submissionLockDataSource());
     }
 }
