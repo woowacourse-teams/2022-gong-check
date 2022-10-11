31efd28b6943e35ee453337f9f1eb66a44962606
cks3066 <cks3066@naver.com>
2022-10-12 00:49:54 +0900

    fix: 스크롤이 내려간 상태에서도 메뉴를 펼칠 수 있도록 수정

--- frontend/src/components/common/SlideMenu/styles.ts
+++ frontend/src/components/common/SlideMenu/styles.ts
@@ -4,10 +4,11 @@ import animation from '@/styles/animation';
 import theme from '@/styles/theme';
 
 const slideMenu = (isShowMenu: boolean | null) => css`
-  position: absolute;
+  position: fixed;
+  top: 64px;
   min-height: 100vh;
   background-color: ${theme.colors.white};
-  z-index: 1000000;
+  z-index: 10;
   width: 100%;
 
   animation: ${isShowMenu === true ? animation.navigatorOpen : animation.navigatorClose} 0.2s ease-out;
