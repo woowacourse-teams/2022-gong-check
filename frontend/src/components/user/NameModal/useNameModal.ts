91fcac310ac4a6db781b861baec4465dc0876ee5
cks3066 <cks3066@naver.com>
2022-10-14 13:44:33 +0900

    feat: 프론트엔드 코드 sse > socket

--- frontend/src/components/user/NameModal/useNameModal.ts
+++ frontend/src/components/user/NameModal/useNameModal.ts
@@ -1,30 +1,8 @@
-import { AxiosError } from 'axios';
 import { useState } from 'react';
-import { useMutation } from 'react-query';
-import { useNavigate } from 'react-router-dom';
 
-import useModal from '@/hooks/useModal';
-import useToast from '@/hooks/useToast';
-
-import apis from '@/apis';
-
-import { ID } from '@/types';
-
-import errorMessage from '@/constants/errorMessage';
-
-const useNameModal = (jobId: ID) => {
-  const { openToast } = useToast();
-  const { closeModal } = useModal();
-
+const useNameModal = (completeJobs: (name: string) => void) => {
   const [name, setName] = useState('');
   const [isDisabledButton, setIsDisabledButton] = useState(true);
-
-  const { mutate: postJobComplete } = useMutation(() => apis.postJobComplete(jobId, name), {
-    onError: (err: AxiosError<{ errorCode: keyof typeof errorMessage }>) => {
-      openToast('ERROR', errorMessage[`${err.response?.data.errorCode!}`]);
-      closeModal();
-    },
-  });
 
   const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const isTyped = !!e.target.value;
@@ -33,7 +11,7 @@ const useNameModal = (jobId: ID) => {
   };
 
   const onClickButton = () => {
-    postJobComplete();
+    completeJobs(name);
   };
 
   return { onChangeInput, onClickButton, isDisabledButton, name };
--- frontend/src/pages/user/TaskList/useTaskList.tsx
+++ frontend/src/pages/user/TaskList/useTaskList.tsx
@@ -1,7 +1,7 @@
 import { Stomp } from '@stomp/stompjs';
 import { useEffect, useState } from 'react';
 import { useRef } from 'react';
-import { useMutation, useQuery } from 'react-query';
+import { useQuery } from 'react-query';
 import { useLocation, useParams } from 'react-router-dom';
 import SockJS from 'sockjs-client';
 
@@ -11,6 +11,7 @@ import NameModal from '@/components/user/NameModal';
 import useGoPreviousPage from '@/hooks/useGoPreviousPage';
 import useModal from '@/hooks/useModal';
 import useSectionCheck from '@/hooks/useSectionCheck';
+import useToast from '@/hooks/useToast';
 
 import apis from '@/apis';
 
@@ -25,7 +26,8 @@ const useTaskList = () => {
   const location = useLocation();
   const locationState = location.state as { jobName: string };
 
-  const { openModal } = useModal();
+  const { openModal, closeModal } = useModal();
+  const { openToast } = useToast();
 
   const { goPreviousPage } = useGoPreviousPage();
 
@@ -44,8 +46,6 @@ const useTaskList = () => {
   const { data: spaceData } = useQuery(['space', jobId], () => apis.getSpace(spaceId));
   const { data: runningTasksData } = useQuery(['runningTask'], () => apis.getRunningTasks(jobId));
 
-  const { mutate: postSectionAllCheck } = useMutation((sectionId: ID) => apis.postSectionAllCheckTask(sectionId));
-
   const { sectionsAllCheckMap, totalCount, checkedCount, percent, isAllChecked } = useSectionCheck(
     sectionsData?.sections || []
   );
@@ -60,7 +60,7 @@ const useTaskList = () => {
         detail="확인 버튼을 누르면 제출됩니다."
         placeholder="이름을 입력해주세요."
         buttonText="확인"
-        jobId={jobId}
+        completeJobs={completeJobs}
       />
     );
   };
@@ -69,8 +69,9 @@ const useTaskList = () => {
     openModal(<DetailInfoModal name={section.name} imageUrl={section.imageUrl} description={section.description} />);
   };
 
-  const onClickSectionAllCheck = (sectionId: ID) => {
-    postSectionAllCheck(sectionId);
+  const completeJobs = (author: string) => {
+    if (!stomp.current) return;
+    stomp.current.send(`/app/jobs/${jobId}/complete`, {}, JSON.stringify({ author }));
   };
 
   const flipTaskCheck = (taskId: ID) => {
@@ -78,13 +79,24 @@ const useTaskList = () => {
     stomp.current.send(`/app/jobs/${jobId}/tasks/flip`, {}, JSON.stringify({ taskId }));
   };
 
+  const onClickSectionAllCheck = (sectionId: ID) => {
+    if (!stomp.current) return;
+    stomp.current.send(`/app/jobs/${jobId}/sections/checkAll`, {}, JSON.stringify({ sectionId }));
+  };
+
   useEffect(() => {
-    const sock = new SockJS(`${API_URL}/ws/connect`);
+    const sock = new SockJS(`${API_URL}/ws-connect`);
     stomp.current = Stomp.over(sock);
 
     stomp.current.connect({}, () => {
-      stomp.current.subscribe(`/topic/${jobId}`, (data: any) => {
+      stomp.current.subscribe(`/topic/jobs/${jobId}`, (data: any) => {
         setSectionsData(JSON.parse(data.body));
+      });
+
+      stomp.current.subscribe(`/topic/jobs/${jobId}/complete`, (data: any) => {
+        closeModal();
+        openToast('SUCCESS', '해당 체크리스트는 제출되었습니다.');
+        goPreviousPage();
       });
     });
 
@@ -92,12 +104,6 @@ const useTaskList = () => {
       stomp.current.disconnect();
       sock.close();
     };
-
-    // sse.addEventListener('submit', () => {
-    //   closeModal();
-    //   openToast('SUCCESS', '해당 체크리스트는 제출되었습니다.');
-    //   goPreviousPage();
-    // });
   }, []);
 
   useEffect(() => {
