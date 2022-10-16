e8b9bdea45f7bb21641f477ff2f9241998a5b544
intae92 <kangit2013@gmail.com>
2022-10-17 02:15:14 +0900

    fix: 깃헙 프로필 가져오는 api에서 생기는 이슈
    TypeError: Cannot read properties of undefined(reading 'cancelToken')
    에러가 에러 바운더리에 걸려 존재하지 않는 페이지(NotFound)로 넘어간다. 이 문제에 대해 추후 다시 확인을 해봐야 할 꺼 같다

--- frontend/src/components/common/Profile/index.tsx
+++ frontend/src/components/common/Profile/index.tsx
@@ -5,7 +5,10 @@ import apis from '@/apis';
 import styles from './styles';
 
 const Profile: React.FC = () => {
-  const { data: profileData } = useQuery(['hostProfile'], apis.getHostProfile, { suspense: false });
+  const { data: profileData } = useQuery(['hostProfile'], apis.getHostProfile, {
+    suspense: false,
+    useErrorBoundary: false,
+  });
 
   return (
     <div css={styles.profile}>
